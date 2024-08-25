216fa8aeabf0fa5266f6ccab294e36b2c533320c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-25 18:09:48 +0200

    dummy parsing

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 
 #include "tokenize/t_token.h"
+#include "parse/parse.h"
 
 #define SHELL_PROMPT "minishell$ "
 
@@ -9,11 +10,14 @@ int	main(void)
 {
 	char	*input;
 	TokenList tokens;
+	t_command command;
 
 	while (1)
 	{
 		input = readline(SHELL_PROMPT);
 		tokens = tokenize(input);
+		command = parse_command(tokens);
+		(void)command;
 		free(input);
 	}
 }
