fbfeb9781f7a689e960e93b657cb3d392f2558d4
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-25 19:55:32 +0200

    refactor

--- src/command/t_command.h
+++ src/command/t_command.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 
 typedef enum e_command_type t_command_type;
+typedef enum e_logic_node_kind t_logic_node_kind;
 typedef enum e_operator_kind t_operator_kind;
 typedef struct s_logic_node t_logic_node;
 typedef struct s_simple_command t_simple_command;
@@ -18,33 +19,33 @@ enum e_command_type {
     SEPARATOR
 };
 
-typedef enum e_logic_node_kind {
+enum e_logic_node_kind {
     AND,
     OR
-} t_logic_node_kind;
+};
 
-typedef struct s_logic_node {
+struct s_logic_node {
     t_logic_node_kind    kind;
     t_command            *left;
     t_command            *right;
-} t_logic_node;
+};
 
 // NOTE: `words` must be null terminated!
-typedef struct s_simple_command {
+struct s_simple_command {
     int /* ?? */    *redirections;
     char            **words;
-} t_simple_command;
+};
 
-typedef union u_any_command {
+union u_any_command {
     struct s_simple_command    simple;
     struct s_logic_node    simple__;
-} t_any_command;
+};
 
-typedef struct s_command {
-    enum e_command_type    type;
-    struct s_command *left;
-    struct s_command *right;
-    union u_any_command    command;
-} t_command;
+struct s_command {
+    t_command_type    type;
+    t_command *left;
+    t_command *right;
+    t_any_command    command;
+};
 
 #endif // COMMAND_H
