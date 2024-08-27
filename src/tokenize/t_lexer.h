79cae6a90070090d031d4552d409f46a4836cc52
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-27 19:19:59 +0200

    sketch t_lexer

---
+++ src/tokenize/t_lexer.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "t_token_list/t_token_list.h"
+#include <stddef.h>
+
+typedef struct s_lexer
+{
+    const char* source;
+    t_token_list* tokens;
+    size_t current;
+    size_t start;
+} t_lexer;
+
+t_lexer lexer_init(const char* source);
