6a322947996bde9c8e70331d14cb328162ebadf8
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-28 13:32:09 +0200

    parse single char tokens

--- src/tokenize/lexer_scan_next_token.c
+++ src/tokenize/lexer_scan_next_token.c
@@ -1,8 +1,20 @@
+#include "error/t_error.h"
 #include "t_lexer.h"
+
 
 t_error	lexer_scan_next_token(t_lexer *lexer, t_token *out)
 {
-	(void)lexer;
-	(void)out;
-	return (NO_ERROR);
+	char c = lexer->source[lexer->current++];
+
+	if (c == '(')
+	{
+		*out = (t_token){.type = L_PAREN};
+		return (NO_ERROR);
+	}
+	if (c == ')')
+	{
+		*out = (t_token){.type = R_PAREN};
+		return (NO_ERROR);
+	}
+	return (E_UNRECOGNIZED_TOKEN);
 }
