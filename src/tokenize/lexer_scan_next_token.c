c52fa1e5e4a0e8b46a4a68dd9d2f2aa6d39ecb37
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-28 21:54:37 +0200

    sketch double quote lexing but need libft to test

--- src/tokenize/lexer_scan_next_token.c
+++ src/tokenize/lexer_scan_next_token.c
@@ -1,5 +1,22 @@
 #include "error/t_error.h"
 #include "t_lexer.h"
+#include "libft/ft_string.h"
+
+t_error	lexer_scan_double_quote_string(t_lexer *lexer, t_token *out)
+{
+	char* literal;
+
+	while (lexer_peek(lexer) != '"' && lexer->current < lexer->src_len)
+	{
+		lexer_advance(lexer);
+	}
+	if (lexer->current >= lexer->src_len)
+		return E_UNTERMINATED_QUOTE;
+	lexer_advance(lexer);
+	literal = ft_substr(lexer->source, lexer->start + 1, lexer->start - ;lexer->current - 2);
+	*out = (t_token){.type = DOUBLE_QUOTE_STRING, .literal = literal};
+	return NO_ERROR;
+}
 
 t_error	lexer_scan_next_token(t_lexer *lexer, t_token *out)
 {
