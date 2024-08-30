05d0fea992a13b7b014527e33ea43ef10f49fc33
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-30 13:59:03 +0200

    enable error logging

--- src/log/log.c
+++ src/log/log.c
@@ -2,7 +2,14 @@
 
 #include <stdio.h>
 
-void log_token(t_token token)
+void log_token(t_token token) { printf("%s", token_repr(token)); }
+
+void log_error(t_error err)
 {
-	printf("%s", token_repr(token));
+    if (err == NO_ERROR)
+    {
+        printf("No error\n");
+        return;
+    }
+    printf("Error:\t%s\n", error_repr(err));
 }
--- src/log/log.h
+++ src/log/log.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "error/t_error.h"
 #include "tokenize/t_token.h"
 
 void log_token(t_token token);
+void log_error(t_error err);
