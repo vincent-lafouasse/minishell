90986f363cb3a1e239ca9dfa09ee28af396f977b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-30 14:16:52 +0200

    refactor tokenize::tokenize() using a `cleanup` function

--- src/tokenize/tokenize.c
+++ src/tokenize/tokenize.c
@@ -7,6 +7,7 @@
 #define WHITESPACE " "
 
 static void skip_whitespace(t_lexer *lexer);
+static void cleanup_exit(t_lexer *lexer, t_error err);
 
 t_token_list *tokenize(const char *input)
 {
@@ -21,15 +22,14 @@ t_token_list *tokenize(const char *input)
         lexer.start = lexer.current;
         err = lexer_scan_next_token(&lexer, &token);
         if (err != NO_ERROR)
-        {
-            log_token_list(lexer.tokens);
-            log_error(err);
-            // cleanup tokens
-            return NULL;
-        }
+            return cleanup_exit(&lexer, err), NULL;
         err = tkl_push_back(&lexer.tokens, token);
+        if (err != NO_ERROR)
+            return cleanup_exit(&lexer, err), NULL;
     }
     tkl_push_back(&lexer.tokens, (t_token){.type = EOF_TOKEN});
+    if (err != NO_ERROR)
+        return cleanup_exit(&lexer, err), NULL;
     log_token_list(lexer.tokens);
     return (lexer.tokens);
 }
@@ -39,4 +39,11 @@ static void skip_whitespace(t_lexer *lexer)
     while (lexer->current < lexer->src_len &&
            ft_strchr(WHITESPACE, lexer_peek(lexer)) != NULL)
         lexer_advance(lexer);
+}
+
+static void cleanup_exit(t_lexer *lexer, t_error err)
+{
+    log_token_list(lexer->tokens);
+    log_error(err);
+    tkl_clear(&lexer->tokens);
 }
