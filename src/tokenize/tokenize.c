e82e82674daf85e405bc28a87d25729eecf1e487
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-08-30 14:05:08 +0200

    refactor tokenize::tokenize using helper functions

--- src/tokenize/tokenize.c
+++ src/tokenize/tokenize.c
@@ -1,34 +1,42 @@
-#include "t_lexer.h"
 #include "tokenize.h"
+#include "libft/string.h"
 #include "log/log.h"
+#include "t_lexer.h"
 #include <stdio.h>
 
-t_token_list	*tokenize(const char *input)
-{
-	t_lexer	lexer;
-	t_token	token;
-	t_error	err;
+#define WHITESPACE " "
 
-	lexer = lexer_init(input);
-	while (lexer.current < lexer.src_len)
-	{
-		if (lexer_peek(&lexer) == ' ')
-		{
-			lexer_advance(&lexer);
-			continue;
-		}
-		lexer.start = lexer.current;
-		err = lexer_scan_next_token(&lexer, &token);
-		if (err != NO_ERROR)
-		{
-			log_token_list(lexer.tokens);
-			printf("Error:\t%s\n", error_repr(err));
-			// cleanup tokens
-			return NULL;
-		}
-		err = tkl_push_back(&lexer.tokens, token);
-	}
-	tkl_push_back(&lexer.tokens, (t_token){.type = EOF_TOKEN});
-	log_token_list(lexer.tokens);
-	return (lexer.tokens);
+static void skip_whitespace(t_lexer *lexer);
+
+t_token_list *tokenize(const char *input)
+{
+    t_lexer lexer;
+    t_token token;
+    t_error err;
+
+    lexer = lexer_init(input);
+    while (lexer.current < lexer.src_len)
+    {
+        skip_whitespace(&lexer);
+        lexer.start = lexer.current;
+        err = lexer_scan_next_token(&lexer, &token);
+        if (err != NO_ERROR)
+        {
+            log_token_list(lexer.tokens);
+            log_error(err);
+            // cleanup tokens
+            return NULL;
+        }
+        err = tkl_push_back(&lexer.tokens, token);
+    }
+    tkl_push_back(&lexer.tokens, (t_token){.type = EOF_TOKEN});
+    log_token_list(lexer.tokens);
+    return (lexer.tokens);
+}
+
+static void skip_whitespace(t_lexer *lexer)
+{
+    while (lexer->current < lexer->src_len &&
+           ft_strchr(WHITESPACE, lexer_peek(lexer)) != NULL)
+        lexer_advance(lexer);
 }
