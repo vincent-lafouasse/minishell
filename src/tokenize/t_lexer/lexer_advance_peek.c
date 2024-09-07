0f0f9940318ca02939a44a217ad62752866e3c3b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-07 18:48:58 +0200

    format

--- src/tokenize/t_lexer/lexer_advance_peek.c
+++ src/tokenize/t_lexer/lexer_advance_peek.c
@@ -1,13 +1,15 @@
 #include "t_lexer.h"
 
-char lexer_advance(t_lexer *lexer) {
+char lexer_advance(t_lexer *lexer)
+{
     if (lexer->current < lexer->src_len)
         return lexer->source[lexer->current++];
     else
         return '\0';
 }
 
-char lexer_peek(t_lexer *lexer) {
+char lexer_peek(t_lexer *lexer)
+{
     if (lexer->current <= lexer->src_len)
         return lexer->source[lexer->current];
     else
