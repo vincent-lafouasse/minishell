cd3c9e3c8ecc097e432ab678f303a18e1754079c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-07 16:13:30 +0200

    boundary checks on lexer_{advance,peek}

--- src/tokenize/t_lexer/lexer_advance_peek.c
+++ src/tokenize/t_lexer/lexer_advance_peek.c
@@ -1,5 +1,15 @@
 #include "t_lexer.h"
 
-char lexer_advance(t_lexer *lexer) { return lexer->source[lexer->current++]; }
+char lexer_advance(t_lexer *lexer) {
+    if (lexer->current < lexer->src_len)
+        return lexer->source[lexer->current++];
+    else
+        return '\0';
+}
 
-char lexer_peek(t_lexer *lexer) { return lexer->source[lexer->current]; }
+char lexer_peek(t_lexer *lexer) {
+    if (lexer->current <= lexer->src_len)
+        return lexer->source[lexer->current];
+    else
+        return '\0';
+}
