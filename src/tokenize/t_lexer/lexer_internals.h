2cdbf0c87c56f4a4f074e587d43191c8e9119401
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-07 15:40:00 +0200

    restructure into lexer internals

--- src/tokenize/t_lexer/lexer_internals.h
+++ src/tokenize/t_lexer/lexer_internals.h
@@ -2,7 +2,15 @@
 
 #include "t_lexer.h"
 
-t_error fill_token(t_token token, t_token *out);
+#define SHELL_BREAK_CHARS "()<>;&| \t\n"
+#define METACHARACTERS "()<>|&"
+
+t_error lexer_scan_metacharacter_token(t_lexer *lexer, t_token *out,
+                                       char current);
+t_error lexer_scan_string(t_lexer *lexer, t_token *out, char stop);
+t_error lexer_scan_word(t_lexer *lexer, t_token *out);
 
 char lexer_advance(t_lexer *lexer);
 char lexer_peek(t_lexer *lexer);
+
+t_error fill_token(t_token token, t_token *out);
--- src/tokenize/t_lexer/lexer_scan_next_token.c
+++ src/tokenize/t_lexer/lexer_scan_next_token.c
@@ -1,19 +1,5 @@
-#include "error/t_error.h"
-#include "libft/ft_string.h"
-#include "libft/string.h"
-#include "t_lexer.h"
-#include "tokenize/t_token.h"
 #include "lexer_internals.h"
-
-#define SHELL_BREAK_CHARS "()<>;&| \t\n"
-#define METACHARACTERS "()<>|&"
-
-static t_error lexer_scan_metacharacter_token(t_lexer *lexer, t_token *out,
-                                              char current);
-static t_error lexer_scan_redirection_operators(t_lexer *lexer, t_token *out,
-                                                char current);
-static t_error lexer_scan_string(t_lexer *lexer, t_token *out, char stop);
-static t_error lexer_scan_word(t_lexer *lexer, t_token *out);
+#include "libft/string.h"
 
 t_error lexer_scan_next_token(t_lexer *lexer, t_token *out)
 {
@@ -25,85 +11,4 @@ t_error lexer_scan_next_token(t_lexer *lexer, t_token *out)
     if (c == '"' || c == '\'')
         return (lexer_scan_string(lexer, out, c));
     return (lexer_scan_word(lexer, out));
-}
-
-static t_error lexer_scan_metacharacter_token(t_lexer *lexer, t_token *out,
-                                              char current)
-{
-    if (current == '(')
-        return (fill_token((t_token){.type = L_PAREN}, out));
-    if (current == ')')
-        return (fill_token((t_token){.type = R_PAREN}, out));
-    if (lexer_peek(lexer) == '|')
-        return (lexer_advance(lexer),
-                fill_token((t_token){.type = OR_OR}, out));
-    else
-        return (fill_token((t_token){.type = PIPE_TOKEN}, out));
-    if (current == '&' && lexer_peek(lexer) == '&')
-        return (lexer_advance(lexer),
-                fill_token((t_token){.type = AND_AND}, out));
-    if (current == '<' || current == '>')
-        return (lexer_scan_redirection_operators(lexer, out, current));
-    return E_UNREACHABLE;
-}
-
-static t_error lexer_scan_redirection_operators(t_lexer *lexer, t_token *out,
-                                                char current)
-{
-    if (current == '<')
-    {
-        if (lexer_peek(lexer) == '<')
-        {
-            lexer_advance(lexer);
-            return (fill_token((t_token){.type = DL_ANGLE_BRACKET}, out));
-        }
-        else
-        {
-            return (fill_token((t_token){.type = L_ANGLE_BRACKET}, out));
-        }
-    }
-    if (current == '>')
-    {
-        if (lexer_peek(lexer) == '>')
-        {
-            lexer_advance(lexer);
-            return (fill_token((t_token){.type = DR_ANGLE_BRACKET}, out));
-        }
-        else
-        {
-            return (fill_token((t_token){.type = R_ANGLE_BRACKET}, out));
-        }
-    }
-    return (E_UNREACHABLE);
-}
-
-static t_error lexer_scan_string(t_lexer *lexer, t_token *out, char stop)
-{
-    char *literal;
-    t_token_type token_type;
-    size_t sz;
-
-    while (lexer_peek(lexer) != stop && lexer->current < lexer->src_len)
-        lexer_advance(lexer);
-    if (lexer->current >= lexer->src_len)
-        return (E_UNTERMINATED_QUOTE);
-    lexer_advance(lexer);
-    sz = lexer->current - lexer->start - 2;
-    literal = ft_substr(lexer->source, lexer->start + 1, sz);
-    token_type = SINGLE_QUOTE_STRING * (stop == '\'') +
-                 DOUBLE_QUOTE_STRING * (stop == '"');
-    return fill_token((t_token){.type = token_type, .literal = literal}, out);
-}
-
-static t_error lexer_scan_word(t_lexer *lexer, t_token *out)
-{
-    char *literal;
-    size_t sz;
-
-    while (ft_strchr(SHELL_BREAK_CHARS, lexer_peek(lexer)) == NULL &&
-           lexer->current < lexer->src_len)
-        lexer_advance(lexer);
-    sz = lexer->current - lexer->start;
-    literal = ft_substr(lexer->source, lexer->start, sz);
-    return fill_token((t_token){.type = WORD, .literal = literal}, out);
 }
