6f9b0e451cbd58461a7210e793f200f4d34840ba
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-07 15:30:24 +0200

    restructure into lexer internals

--- src/tokenize/t_lexer/lexer_internals.h
+++ src/tokenize/t_lexer/lexer_internals.h
@@ -1,6 +1,8 @@
 #pragma once
 
-#include "error/t_error.h"
-#include "../t_token.h"
+#include "t_lexer.h"
 
 t_error fill_token(t_token token, t_token *out);
+
+char lexer_advance(t_lexer *lexer);
+char lexer_peek(t_lexer *lexer);
