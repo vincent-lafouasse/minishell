134d2bbaba1035e01d3d62c4e65ea785c2a431fd
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-07 19:20:44 +0200

    test full pipex input with and without whitespace

--- test/src/lexer_test/tokenize.cpp
+++ test/src/lexer_test/tokenize.cpp
@@ -66,3 +66,80 @@ TEST(Tokenize, Pipex)
     ASSERT_EQ(tokens, nullptr);
     ASSERT_EQ(expected_it, expected.cend());
 }
+
+TEST(Tokenize, BigPipex)
+{
+    t_error err;
+
+    const char *source =
+        "cppcheck < file.cpp | cat | sort | wc | a_cmd >> outfile";
+    std::vector<t_token> expected = {
+        (t_token){.type = WORD, .literal = (char *)"cppcheck"},
+        (t_token){.type = L_ANGLE_BRACKET, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"file.cpp"},
+        (t_token){.type = PIPE_TOKEN, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"cat"},
+        (t_token){.type = PIPE_TOKEN, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"sort"},
+        (t_token){.type = PIPE_TOKEN, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"wc"},
+        (t_token){.type = PIPE_TOKEN, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"a_cmd"},
+        (t_token){.type = DR_ANGLE_BRACKET, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"outfile"},
+        (t_token){.type = EOF_TOKEN, .literal = nullptr},
+    };
+
+    t_token_list *tokens = tokenize(source);
+    ASSERT_NE(tokens, nullptr);
+
+    auto expected_it = expected.cbegin();
+
+    while (expected_it != expected.cend() && tokens)
+    {
+        ASSERT_EQ(expected_it->type, tokens->token.type);
+        ASSERT_STREQ(expected_it->literal, tokens->token.literal);
+        expected_it++;
+        tokens = tokens->next;
+    }
+    ASSERT_EQ(tokens, nullptr);
+    ASSERT_EQ(expected_it, expected.cend());
+}
+
+TEST(Tokenize, BigPipexNoWhitespace)
+{
+    t_error err;
+
+    const char *source = "cppcheck<file.cpp|cat|sort|wc|a_cmd>>outfile";
+    std::vector<t_token> expected = {
+        (t_token){.type = WORD, .literal = (char *)"cppcheck"},
+        (t_token){.type = L_ANGLE_BRACKET, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"file.cpp"},
+        (t_token){.type = PIPE_TOKEN, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"cat"},
+        (t_token){.type = PIPE_TOKEN, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"sort"},
+        (t_token){.type = PIPE_TOKEN, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"wc"},
+        (t_token){.type = PIPE_TOKEN, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"a_cmd"},
+        (t_token){.type = DR_ANGLE_BRACKET, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"outfile"},
+        (t_token){.type = EOF_TOKEN, .literal = nullptr},
+    };
+
+    t_token_list *tokens = tokenize(source);
+    ASSERT_NE(tokens, nullptr);
+
+    auto expected_it = expected.cbegin();
+
+    while (expected_it != expected.cend() && tokens)
+    {
+        ASSERT_EQ(expected_it->type, tokens->token.type);
+        ASSERT_STREQ(expected_it->literal, tokens->token.literal);
+        expected_it++;
+        tokens = tokens->next;
+    }
+    ASSERT_EQ(tokens, nullptr);
+    ASSERT_EQ(expected_it, expected.cend());
+}
