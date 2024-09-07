870924f896a3fad1025580fac4ffa2902ac11785
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-07 19:17:36 +0200

    test pipex style input

--- test/src/lexer_test/tokenize.cpp
+++ test/src/lexer_test/tokenize.cpp
@@ -34,3 +34,35 @@ TEST(Tokenize, Simple)
     ASSERT_EQ(tokens, nullptr);
     ASSERT_EQ(expected_it, expected.cend());
 }
+
+TEST(Tokenize, Pipex)
+{
+    t_error err;
+
+    const char *source = "< infile cmd1 | cmd2 > outfile";
+    std::vector<t_token> expected = {
+        (t_token){.type = L_ANGLE_BRACKET, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"infile"},
+        (t_token){.type = WORD, .literal = (char *)"cmd1"},
+        (t_token){.type = PIPE_TOKEN, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"cmd2"},
+        (t_token){.type = R_ANGLE_BRACKET, .literal = NULL},
+        (t_token){.type = WORD, .literal = (char *)"outfile"},
+        (t_token){.type = EOF_TOKEN, .literal = nullptr},
+    };
+
+    t_token_list *tokens = tokenize(source);
+    ASSERT_NE(tokens, nullptr);
+
+    auto expected_it = expected.cbegin();
+
+    while (expected_it != expected.cend() && tokens)
+    {
+        ASSERT_EQ(expected_it->type, tokens->token.type);
+        ASSERT_STREQ(expected_it->literal, tokens->token.literal);
+        expected_it++;
+        tokens = tokens->next;
+    }
+    ASSERT_EQ(tokens, nullptr);
+    ASSERT_EQ(expected_it, expected.cend());
+}
