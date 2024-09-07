a19f5f781577aa6c59bb8b8049fcfee93d97473e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-07 18:56:16 +0200

    dummy lexer tests

---
+++ test/src/lexer_test/lexer.cpp
@@ -0,0 +1,14 @@
+#include "gtest/gtest.h"
+
+#include <vector>
+
+extern "C"
+{
+#include "tokenize/t_lexer/t_lexer.h"
+#include "tokenize/t_lexer/lexer_internals.h"
+};
+
+TEST(Lexer, Simple)
+{
+    ASSERT_TRUE(true);
+}
