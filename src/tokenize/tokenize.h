d787b6dca90c0b9f0c0aab7595a19ca32e09c47d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-09 11:02:27 +0200

    documentation

--- src/tokenize/tokenize.h
+++ src/tokenize/tokenize.h
@@ -3,4 +3,8 @@
 #include "t_token.h"
 #include "t_token_list/t_token_list.h"
 
+//!  Split a string into tokens, mostly words, strings and operators
+/*!
+  Each token owns a copy of its lexeme (if applicable)
+*/
 t_token_list *tokenize(const char *input);
