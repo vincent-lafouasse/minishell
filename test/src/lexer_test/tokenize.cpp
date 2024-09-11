1a7f568800a80b9b29ef6943ff434a05b279863a
Jonathan <imbitquence@gmail.com>
2024-09-11 19:54:06 +0200

    add more lexer tests

--- test/src/lexer_test/tokenize.cpp
+++ test/src/lexer_test/tokenize.cpp
@@ -34,6 +34,167 @@ static t_token Token(t_token_type type, const char *literal)
     return (t_token){.type = type, .literal = const_cast<char *>(literal)};
 }
 
+TEST(Tokenize, Word)
+{
+    const char *source = "hello";
+    std::vector<t_token> expected = {
+        Token(WORD, "hello"),
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
+TEST(Tokenize, SingleQuotedString)
+{
+    const char *source = "'hello'";
+    std::vector<t_token> expected = {
+        Token(SINGLE_QUOTE_STRING, "hello"),
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
+TEST(Tokenize, DoubleQuotedString)
+{
+    const char *source = "\"hello\"";
+    std::vector<t_token> expected = {
+        Token(DOUBLE_QUOTE_STRING, "hello"),
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
+TEST(Tokenize, WordFollowedByUnterminatedStringIsInvalid)
+{
+    const char *source = "hello \"world";
+
+    t_token_list *tokens = tokenize(source);
+    ASSERT_EQ(tokens, nullptr);
+}
+
+TEST(Tokenize, WhitespaceSeperatedWords)
+{
+    const char *source = "hello world";
+    std::vector<t_token> expected = {
+        Token(WORD, "hello"),
+        Token(WORD, "world"),
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
+TEST(Tokenize, WhitespaceSeperatedWordAndString)
+{
+    const char *source = "hello 'world'";
+    std::vector<t_token> expected = {
+        Token(WORD, "hello"),
+        Token(SINGLE_QUOTE_STRING, "world"),
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
+TEST(Tokenize, TabSeperatedWords)
+{
+    const char *source = "hello\tworld";
+    std::vector<t_token> expected = {
+        Token(WORD, "hello"),
+        Token(WORD, "world"),
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
+TEST(Tokenize, Whitespaces)
+{
+    const char *source = "   ";
+    std::vector<t_token> expected = {
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
+TEST(Tokenize, Tabs)
+{
+    const char *source = "\t\t\t";
+    std::vector<t_token> expected = {
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
+TEST(Tokenize, Operator)
+{
+    const char *source = "<";
+    std::vector<t_token> expected = {
+        Token(L_ANGLE_BRACKET, nullptr),
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
+TEST(Tokenize, SingleQuotedOperator)
+{
+    const char *source = "'<'";
+    std::vector<t_token> expected = {
+        Token(SINGLE_QUOTE_STRING, "<"),
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
+TEST(Tokenize, ConcatenatedWordsAndStrings)
+{
+    const char *source = "e\"c\"h'o'";
+    std::vector<t_token> expected = {
+        Token(WORD, "e"),
+        Token(DOUBLE_QUOTE_STRING, "c"),
+        Token(WORD, "h"),
+        Token(SINGLE_QUOTE_STRING, "o"),
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
+TEST(Tokenize, ConcatenatedSentence)
+{
+    const char *source = "e\"c\"h'o' hello \"world\"";
+    std::vector<t_token> expected = {
+        Token(WORD, "e"),
+        Token(DOUBLE_QUOTE_STRING, "c"),
+        Token(WORD, "h"),
+        Token(SINGLE_QUOTE_STRING, "o"),
+        Token(WORD, "hello"),
+        Token(DOUBLE_QUOTE_STRING, "world"),
+        Token(EOF_TOKEN, nullptr),
+    };
+
+    t_token_list *tokens = tokenize(source);
+    assert_tkl_equality(tokens, expected);
+}
+
 TEST(Tokenize, SimpleCommandWithOutputRedirection)
 {
     const char *source = "hello > world";
