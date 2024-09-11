81419b5b740f2a79ceb39d8e40be59a0a487cdb1
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-09-11 22:24:51 +0200

    format

--- test/src/lexer_test/tokenize.cpp
+++ test/src/lexer_test/tokenize.cpp
@@ -1,7 +1,7 @@
 #include "gtest/gtest.h"
 
-#include <vector>
 #include <string>
+#include <vector>
 
 extern "C"
 {
@@ -12,28 +12,26 @@ extern "C"
 struct TokenList
 {
     TokenList();
-    TokenList(const std::string& src);
-    TokenList(const std::vector<t_token>& src);
+    TokenList(const std::string &src);
+    TokenList(const std::vector<t_token> &src);
     ~TokenList();
 
-    t_token_list* head;
+    t_token_list *head;
 };
 
 TokenList::TokenList() : head(nullptr) {}
 
-TokenList::TokenList(const std::string& src) : head(tokenize(src.c_str())) {}
+TokenList::TokenList(const std::string &src) : head(tokenize(src.c_str())) {}
 
-TokenList::TokenList(const std::vector<t_token>& src) : head(nullptr) {
-    for (const t_token& token: src)
+TokenList::TokenList(const std::vector<t_token> &src) : head(nullptr)
+{
+    for (const t_token &token : src)
     {
         tkl_push_back(&head, token);
     }
 }
 
-TokenList::~TokenList()
-{
-    tkl_clear(&head);
-}
+TokenList::~TokenList() { tkl_clear(&head); }
 
 static void assert_tkl_equality(const t_token_list *tokens,
                                 const std::vector<t_token> &expected_tokens)
@@ -194,10 +192,8 @@ TEST(Tokenize, ConcatenatedWordsAndStrings)
 {
     const char *source = "e\"c\"h'o'";
     std::vector<t_token> expected = {
-        Token(WORD, "e"),
-        Token(DOUBLE_QUOTE_STRING, "c"),
-        Token(WORD, "h"),
-        Token(SINGLE_QUOTE_STRING, "o"),
+        Token(WORD, "e"),          Token(DOUBLE_QUOTE_STRING, "c"),
+        Token(WORD, "h"),          Token(SINGLE_QUOTE_STRING, "o"),
         Token(EOF_TOKEN, nullptr),
     };
 
@@ -209,12 +205,9 @@ TEST(Tokenize, ConcatenatedSentence)
 {
     const char *source = "e\"c\"h'o' hello \"world\"";
     std::vector<t_token> expected = {
-        Token(WORD, "e"),
-        Token(DOUBLE_QUOTE_STRING, "c"),
-        Token(WORD, "h"),
-        Token(SINGLE_QUOTE_STRING, "o"),
-        Token(WORD, "hello"),
-        Token(DOUBLE_QUOTE_STRING, "world"),
+        Token(WORD, "e"),          Token(DOUBLE_QUOTE_STRING, "c"),
+        Token(WORD, "h"),          Token(SINGLE_QUOTE_STRING, "o"),
+        Token(WORD, "hello"),      Token(DOUBLE_QUOTE_STRING, "world"),
         Token(EOF_TOKEN, nullptr),
     };
 
@@ -240,10 +233,14 @@ TEST(Tokenize, Pipex)
 {
     const char *source = "< infile cmd1 | cmd2 > outfile";
     std::vector<t_token> expected = {
-        Token(L_ANGLE_BRACKET, nullptr), Token(WORD, "infile"),
-        Token(WORD, "cmd1"),             Token(PIPE_TOKEN, nullptr),
-        Token(WORD, "cmd2"),             Token(R_ANGLE_BRACKET, nullptr),
-        Token(WORD, "outfile"),          Token(EOF_TOKEN, nullptr),
+        Token(L_ANGLE_BRACKET, nullptr),
+        Token(WORD, "infile"),
+        Token(WORD, "cmd1"),
+        Token(PIPE_TOKEN, nullptr),
+        Token(WORD, "cmd2"),
+        Token(R_ANGLE_BRACKET, nullptr),
+        Token(WORD, "outfile"),
+        Token(EOF_TOKEN, nullptr),
     };
 
     t_token_list *tokens = tokenize(source);
