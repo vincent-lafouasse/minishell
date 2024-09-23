8833cb0a7dd79a51b4a2f0049a83b5ac841ab0cd
bitquence <imbitquence@gmail.com>
2024-09-23 19:09:11 +0200

    begin grammar processor

---
+++ aux/grammar_processor/corrected_bash_grammar.y
@@ -0,0 +1,45 @@
+program          : complete_command
+                 ;
+complete_command : pipeline
+                 | complete_command AND_IF pipeline
+                 | complete_command OR_IF  pipeline
+                 ;
+pipeline         :              command
+                 | pipeline '|' command
+                 ;
+command          : simple_command
+                 | subshell
+                 | subshell redirect_list
+                 ;
+subshell         : '(' complete_command ')'
+                 ;
+simple_command   : cmd_prefix WORD cmd_suffix
+                 | cmd_prefix WORD
+                 | cmd_prefix
+                 | WORD cmd_suffix
+                 | WORD
+                 ;
+cmd_prefix       :            io_redirect
+                 | cmd_prefix io_redirect
+                 ;
+cmd_suffix       :            io_redirect
+                 | cmd_suffix io_redirect
+                 |            WORD
+                 | cmd_suffix WORD
+                 ;
+redirect_list    :               io_redirect
+                 | redirect_list io_redirect
+                 ;
+io_redirect      :             io_file
+                 |             io_here
+                 ;
+io_file          : '<'       filename
+                 | '>'       filename
+                 | DGREAT    filename
+                 ;
+filename         : WORD
+                 ;
+io_here          : DLESS     here_end
+                 ;
+here_end         : WORD
+                 ;
---
+++ aux/grammar_processor/src/main.rs
@@ -0,0 +1,65 @@
+#![allow(dead_code)]
+#![allow(unused_imports)]
+use std::collections::{HashMap, HashSet};
+use std::fs;
+
+type Symbol = /* Either<NonTerminal, Terminal> */ String;
+
+#[derive(Debug)]
+struct Grammar {
+    /// key is always NT in CFG
+    rules: HashMap<Symbol, HashSet<Vec<Symbol>>>,
+}
+
+impl Grammar {
+    pub fn from_yacc_file<R: std::io::Read>(mut reader: R) -> Grammar {
+        let contents_owned = {
+            let mut buf = String::new();
+            reader.read_to_string(&mut buf).expect("can no read to string");
+            buf
+        };
+
+        Self::from_yacc_text(&contents_owned)
+    }
+
+    pub fn from_yacc_text(mut contents: &str) -> Grammar {
+        let mut this = Self {
+            rules: HashMap::new(),
+        };
+
+        contents = contents
+            .split("%%")
+            .skip(1)
+            .next()
+            .expect("nothing after (possibly) '%%'");
+        let rules: Vec<&str> = contents.split(";").collect();
+        for rule in rules {
+            match &rule.split(":").collect::<Vec<_>>()[..] {
+                &[variable, productions] => {
+                    let handle = this.rules.entry(variable.trim().to_owned()).or_default();
+                    for branch in productions.split("|").map(|r| r.trim()) {
+                        let did_not_exist = handle.insert(
+                            branch.split_whitespace().map(|s| s.to_owned()).collect::<Vec<String>>()
+                        );
+                        if !did_not_exist {
+                            eprintln!("warning: encountered duplicate branch");
+                        }
+                    }
+                }
+                unknown => {
+                    if unknown == ["\n"] {
+                        continue;
+                    }
+                    eprintln!("warning: invalid production");
+                }
+            };
+        }
+
+        this
+    }
+}
+
+fn main() {
+    let yacc_grammar = include_str!("../../filtered_bash_grammar.y");
+    let grammar = Grammar::from_yacc_text(yacc_grammar);
+}
