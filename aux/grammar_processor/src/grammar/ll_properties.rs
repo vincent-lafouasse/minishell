3f3f7833974adbcf31f7f3469a6d296778d1904f
bitquence <imbitquence@gmail.com>
2024-09-24 18:26:15 +0200

    implement follow table generation

--- aux/grammar_processor/src/grammar/ll_properties.rs
+++ aux/grammar_processor/src/grammar/ll_properties.rs
@@ -5,8 +5,12 @@ use super::{Grammar, Symbol};
 #[derive(Debug)]
 pub struct LLProperties {
     first: HashMap<Symbol, HashSet<Symbol>>,
-    //follow: HashMap<Symbol, HashSet<Symbol>>,
+    follow: HashMap<Symbol, HashSet<Symbol>>,
     //first_augmented: HashMap<(Symbol, Symbol), HashMap<>>
+}
+
+fn owned_cloned_union<T: Clone +Eq + std::hash::Hash>(lhs: &HashSet<T>, rhs: &HashSet<T>) -> HashSet<T> {
+    lhs.union(rhs).map(Clone::clone).collect()
 }
 
 impl LLProperties {
@@ -82,12 +86,79 @@ impl LLProperties {
         first
     }
 
+    fn compute_follow(
+        grammar: &Grammar,
+        first: &HashMap<Symbol, HashSet<Symbol>>,
+    ) -> HashMap<Symbol, HashSet<Symbol>> {
+        let mut follow = HashMap::new();
+
+        for non_terminal in grammar.non_terminals().clone() {
+            follow.insert(non_terminal, HashSet::new());
+        }
+        follow.insert(grammar.start_symbol().clone(), HashSet::from(["$".to_owned()]));
+
+        // for each A∈NT do;
+        //     FOLLOW(A) ← ∅;
+        // end;
+        //
+        // FOLLOW(S) ← {eof};
+        //
+        // while (FOLLOW sets are still changing) do;
+        //     for each p∈P of the form A→β1β2···βk do;
+        //         TRAILER ← FOLLOW(A);
+        //         for i ← k down to 1 do;
+        //             if βi ∈ NT then begin;
+        //                 FOLLOW(βi) ← FOLLOW(βi) ∪ TRAILER;
+        //                 if ε ∈ FIRST(βi)
+        //                     then TRAILER ← TRAILER ∪ (FIRST(βi) − ε);
+        //                     else TRAILER ← FIRST(βi);
+        //             end;
+        //             else TRAILER ← FIRST(βi); // is {βi}
+        //         end;
+        //     end;
+        // end;
+        let mut changing = true;
+        while changing {
+            changing = false;
+            for (variable, production) in grammar
+                .rules()
+                .iter()
+                .map(|(symbol, productions)| productions.iter().map(|p| (symbol.clone(), p)))
+                .flatten()
+            {
+                let mut trailer = follow.get_mut(&variable).expect("trailer uninitialized").clone();
+                for sym in production.into_iter().rev() {
+                    if grammar.non_terminals().contains(sym) {
+                        let entry = follow.get_mut(sym).expect(&format!("entry does not exist for symbol: {sym}"));
+                        let union = owned_cloned_union(entry, &trailer);
+                        if entry != &union {
+                            changing = true;
+                        }
+                        *entry = union;
+                        let mut first_of_sym = first.get(sym).unwrap().clone();
+                        if first_of_sym.contains("") {
+                            first_of_sym.remove("");
+                            trailer = owned_cloned_union(&trailer, &first_of_sym);
+                        } else {
+                            trailer = first_of_sym;
+                        }
+                    } else {
+                        trailer = HashSet::from([sym.clone()]);
+                    }
+                }
+            }
+        }
+
+        follow
+    }
+
     pub fn compute(grammar: &Grammar) -> Self {
         let first = Self::compute_first(grammar);
+        let follow = Self::compute_follow(grammar, &first);
 
         dbg!(LLProperties {
             first,
-            //follow: unimplemented!(),
+            follow,
         })
     }
 }
