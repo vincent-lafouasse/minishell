5814553e14716c2b893347bafb53516417318afe
bitquence <imbitquence@gmail.com>
2024-10-01 20:55:12 +0200

    extract function pointer types into typedefs

--- src/parse/t_parser.h
+++ src/parse/t_parser.h
@@ -13,13 +13,16 @@ struct s_parser
     t_error err;
 };
 
+
+typedef t_symbol (*t_production)(t_parser *);
+
 t_parser parser_new(const t_token_list *tokens);
 bool parser_matches(const t_parser *parser, t_token_type ty);
 bool parser_matches_one_of(const t_parser *parser, const t_token_type types[],
                            size_t type_count);
 bool parser_accept_push(t_parser *parser, t_token_type type,
                         t_symbol_array *out);
-bool parser_produce_push(t_parser *parser, t_symbol (*prod)(t_parser *),
+bool parser_produce_push(t_parser *parser, t_production prod,
                          t_symbol_array *out);
 const t_token *parser_peek_token(const t_parser *parser);
 t_token parser_advance_token(t_parser *parser);
