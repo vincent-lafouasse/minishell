de5634cb5f78a51320b6050aaac9e53b020b5754
bitquence <imbitquence@gmail.com>
2024-10-01 21:24:46 +0200

    begin t_command module

--- src/parse/t_command/t_command.h
+++ src/parse/t_command/t_command.h
@@ -1,5 +1,50 @@
 #pragma once
 
+#include "word/t_word_list/t_word_list.h"
+#include "redirection/t_redir_list/t_redir_list.h"
+#include <stdbool.h>
+
+typedef bool (*t_truth_table)(bool, bool);
+
+typedef enum e_command_type {
+	PIPELINE_CMD,
+	CONDITIONAL_CMD,
+	SUBSHELL_CMD,
+	SIMPLE_CMD,
+} t_command_type;
+
+typedef struct s_pipeline t_pipeline;
+typedef struct s_conditional t_conditional;
+typedef struct s_subshell t_subshell;
+typedef struct s_simple t_simple;
+
 typedef struct s_command {
-    int a;
+	t_command_type type;
+	union {
+		t_pipeline *pipeline;
+		t_conditional *conditional;
+		t_subshell *subshell;
+		t_simple *simple;
+	};
 } t_command;
+
+typedef struct s_pipeline {
+	t_command first;
+	t_command second;
+} t_pipeline;
+
+typedef struct s_conditional {
+	t_truth_table condition;
+	t_command first;
+	t_command second;
+} t_conditional;
+
+typedef struct s_subshell {
+	t_command cmd;
+	t_redir_list *redirections
+} t_subshell;
+
+typedef struct s_simple {
+	t_word_list	*words;
+	t_redir_list *redirections
+} t_simple;
