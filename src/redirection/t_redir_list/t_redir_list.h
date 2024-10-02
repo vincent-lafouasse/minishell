38790acf1bbd25dfaacec028d39dfa2399cb1b04
bitquence <imbitquence@gmail.com>
2024-10-02 17:32:27 +0200

    correct redir list argument name

--- src/redirection/t_redir_list/t_redir_list.h
+++ src/redirection/t_redir_list/t_redir_list.h
@@ -32,16 +32,16 @@ typedef struct s_redir_list
 } t_redir_list;
 
 t_redir_list *rdl_new(t_redirect redir /* ! */);
-t_redir_list *rdl_clone(const t_redir_list *words);
+t_redir_list *rdl_clone(const t_redir_list *rdl);
 
-size_t rdl_len(const t_redir_list *words);
+size_t rdl_len(const t_redir_list *rdl);
 
 // used in reduction.c
-t_error rdl_push_back(t_redir_list **words, t_redirect redir);
-void rdl_push_back_link(t_redir_list **words, t_redir_list *link);
-t_redir_list *rdl_pop(t_redir_list **words);
+t_error rdl_push_back(t_redir_list **rdl, t_redirect redir);
+void rdl_push_back_link(t_redir_list **rdl, t_redir_list *link);
+t_redir_list *rdl_pop(t_redir_list **rdl);
 
-void rdl_delone(t_redir_list **words, t_destructor del);
-void rdl_clear(t_redir_list **words, t_destructor del);
+void rdl_delone(t_redir_list **rdl, t_destructor del);
+void rdl_clear(t_redir_list **rdl, t_destructor del);
 
-char **rdl_into_word_array(t_redir_list **words);
+char **rdl_into_word_array(t_redir_list **rdl);
