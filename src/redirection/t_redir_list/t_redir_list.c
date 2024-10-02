fc7cf9aa7103577ee37980672e61fa934ddf94df
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-02 17:06:49 +0200

    fix rdl_new and rdl_push_back specs

--- src/redirection/t_redir_list/t_redir_list.c
+++ src/redirection/t_redir_list/t_redir_list.c
@@ -2,14 +2,14 @@
 
 #include <stdlib.h>
 
-t_redir_list *rdl_new(char *contents)
+t_redir_list *rdl_new(t_redirect redir)
 {
 	t_redir_list* out;
 
 	out = malloc(sizeof(*out));
 	if (!out)
 		return NULL;
-	*out = (t_redir_list){.redirect = {.filename = contents}, .next = NULL};
+	*out = (t_redir_list){.redirect = redir, .next = NULL};
 	return out;
 }
 
@@ -31,9 +31,9 @@ void rdl_push_back_link(t_redir_list **words, t_redir_list *link)
 	last->next = link;
 }
 
-t_error rdl_push_back(t_redir_list **words, char *contents)
+t_error rdl_push_back(t_redir_list **words, t_redirect redir)
 {
-	t_redir_list* new_last = rdl_new(contents);
+	t_redir_list* new_last = rdl_new(redir);
 
 	if (new_last == NULL)
 		return E_OOM;
