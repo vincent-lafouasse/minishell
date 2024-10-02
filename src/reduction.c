3a696f60a6e3dc7eeca5f2825e47198fb960bd17
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-02 15:11:15 +0200

    format

--- src/reduction.c
+++ src/reduction.c
@@ -1,68 +1,72 @@
-#include "parse/t_symbol.h"
 #include "parse/t_command/t_command.h"
+#include "parse/t_symbol.h"
 #include <stdlib.h>
 
 typedef struct s_symbol_stack
 {
-	t_symbol* sym;
-	struct s_symbol_stack* next;
-} t_symbol_stack;
+	t_symbol				*sym;
+	struct s_symbol_stack	*next;
+}							t_symbol_stack;
 
-t_symbol_stack* ss_new(t_symbol* sym)
+t_symbol_stack	*ss_new(t_symbol *sym)
 {
-	t_symbol_stack* out;
+	t_symbol_stack	*out;
 
 	out = malloc(sizeof(*out));
 	if (out == NULL)
-		return NULL;
+		return (NULL);
 	*out = (t_symbol_stack){.sym = sym, .next = NULL};
-	return out;
+	return (out);
 }
 
-void ss_push_link(t_symbol_stack** stack_p, t_symbol_stack* node)
+void	ss_push_link(t_symbol_stack **stack_p, t_symbol_stack *node)
 {
+	t_symbol_stack	*old_head;
+
 	if (*stack_p == NULL)
 	{
 		*stack_p = node;
-		return;
+		return ;
 	}
-
-	t_symbol_stack* old_head = *stack_p;
+	old_head = *stack_p;
 	*stack_p = node;
 	node->next = old_head;
 }
 
-t_symbol_stack* ss_pop_link(t_symbol_stack** stack_p)
+t_symbol_stack	*ss_pop_link(t_symbol_stack **stack_p)
 {
-	if (*stack_p == NULL)
-		return NULL;
+	t_symbol_stack	*out;
 
-	t_symbol_stack* out = *stack_p;
+	if (*stack_p == NULL)
+		return (NULL);
+	out = *stack_p;
 	*stack_p = (*stack_p)->next;
 	out->next = NULL;
-	return out;
+	return (out);
 }
 
-bool ss_push(t_symbol_stack **stack_p, t_symbol* sym)
+bool	ss_push(t_symbol_stack **stack_p, t_symbol *sym)
 {
-	t_symbol_stack* new_head = ss_new(sym);
+	t_symbol_stack	*new_head;
 
+	new_head = ss_new(sym);
 	if (new_head == NULL)
-		return false;
+		return (false);
 	ss_push_link(stack_p, new_head);
-	return true;
+	return (true);
 }
 
-t_symbol* ss_pop(t_symbol_stack** stack_p)
+t_symbol	*ss_pop(t_symbol_stack **stack_p)
 {
-	t_symbol_stack* head = ss_pop_link(stack_p);
+	t_symbol_stack	*head;
+	t_symbol		*out;
 
+	head = ss_pop_link(stack_p);
 	if (head == NULL)
-		return NULL;
-
-	t_symbol* out = head->sym;
+		return (NULL);
+	out = head->sym;
 	free(head);
-	return out;
+	return (out);
 }
 
-t_command reduce_simple_command(t_symbol *root);
+t_command					reduce_simple_command(t_symbol *root);
