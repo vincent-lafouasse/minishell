abde631ae847db6a812537a0a26e479fcd02d56d
bitquence <imbitquence@gmail.com>
2024-10-03 17:56:44 +0200

    sketch parser integration test cases

--- test/src/parser_test/parser_test.cpp
+++ test/src/parser_test/parser_test.cpp
@@ -41,6 +41,20 @@ bool command_eq(t_command a, t_command b) {
 	assert(false);
 }
 
+TEST(ParserIntegration, SimpleWord)
+{
+	const char *input = "echo";
+	t_command actual;
+	t_command expected;
+	t_error err;
+
+	err = parse(input, &actual);
+	ASSERT_EQ(err, NO_ERROR);
+
+	expected = command_new_simple(Words({"echo"}), nullptr);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
+
 TEST(ParserIntegration, SimpleWords)
 {
 	const char *input = "echo hello world";
@@ -54,3 +68,53 @@ TEST(ParserIntegration, SimpleWords)
 	expected = command_new_simple(Words({"echo", "hello", "world"}), nullptr);
 	ASSERT_TRUE(command_eq(actual, expected));
 }
+
+TEST(ParserIntegration, SimpleWordsAndTrailingInputRedirection) {}
+TEST(ParserIntegration, SimpleWordsAndTrailingOutputRedirection) {}
+TEST(ParserIntegration, SimpleWordsAndTrailingAppendRedirection) {}
+TEST(ParserIntegration, SimpleWordsAndTrailingHereDocument) {}
+TEST(ParserIntegration, SimpleWordsAndAllTrailingRedirections) {}
+
+TEST(ParserIntegration, SimpleWordsAndLeadingInputRedirection) {}
+TEST(ParserIntegration, SimpleWordsAndLeadingOutputRedirection) {}
+TEST(ParserIntegration, SimpleWordsAndLeadingAppendRedirection) {}
+TEST(ParserIntegration, SimpleWordsAndLeadingHereDocument) {}
+TEST(ParserIntegration, SimpleWordsAndAllLeadingRedirections) {}
+
+TEST(ParserIntegration, SimpleWordsMixedWithAllLeadingRedirections) {}
+
+TEST(ParserIntegration, SimpleRedirection) {}
+TEST(ParserIntegration, SimpleAllRedirections) {}
+
+TEST(ParserIntegration, SimplePipeline) {}
+TEST(ParserIntegration, MultiPipeline) {}
+TEST(ParserIntegration, PipelineAndRedirections) {}
+TEST(ParserIntegration, PipelineWithTrailingLeadingRedirections) {}
+TEST(ParserIntegration, MultiPipelineWithTrailingLeadingRedirections) {}
+
+TEST(ParserIntegration, SimpleSubshell) {}
+TEST(ParserIntegration, SubshellWithPipeline) {}
+TEST(ParserIntegration, SubshellWithTrailingRedirection) {}
+TEST(ParserIntegration, SubshellWithTrailingRedirections) {}
+TEST(ParserIntegration, SubshellInPipeline) {}
+TEST(ParserIntegration, SimpleNestedSubshell) {}
+TEST(ParserIntegration, NestedSubshellPipelines) {}
+
+TEST(ParserIntegration, SimpleCommandOrConditional) {}
+TEST(ParserIntegration, SimpleCommandAndConditional) {}
+TEST(ParserIntegration, ManySimplesOrConditional) {}
+TEST(ParserIntegration, ManySimplesAndConditional) {}
+TEST(ParserIntegration, PipelinesAndConditional) {}
+TEST(ParserIntegration, PipelinesOrConditional) {}
+TEST(ParserIntegration, SimplesAndSubshellsAndConditional) {}
+TEST(ParserIntegration, SubshellsAndPipelinesConditional) {}
+
+TEST(ParserIntegration, RejectsRedirectionWithoutWord) {}
+TEST(ParserIntegration, RejectsSubshellWithLeadingRedirections) {}
+TEST(ParserIntegration, RejectsNoMatchingSubshellParen) {}
+TEST(ParserIntegration, RejectsUnterminatedPipeline) {}
+TEST(ParserIntegration, RejectsUnterminatedSubshell) {}
+TEST(ParserIntegration, RejectsRedirectionWithNoFollowingWord) {}
+TEST(ParserIntegration, RejectsEmptySubshell) {}
+TEST(ParserIntegration, RejectsEmptySubshellWithTrailingRedirection) {}
+TEST(ParserIntegration, RejectsWtfWithLeadingRedirectedSubshell) {}
