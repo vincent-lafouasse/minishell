900c8fad5cd3e252b7b8ce0e0dc5d514d89d95ac
bitquence <imbitquence@gmail.com>
2024-10-03 16:26:57 +0200

    decouple `log` module

---
+++ src/log/log_token.c
@@ -0,0 +1,21 @@
+#include "log.h"
+
+#include <stdio.h>
+
+void log_token(t_token token)
+{
+    printf("%s", token_repr(token));
+    if (token.literal)
+        printf("\t%s", token.literal);
+}
+
+void log_token_list(const t_token_list *tkl)
+{
+    while (tkl)
+    {
+        printf("token:\t");
+        log_token(tkl->token);
+        printf("\n");
+        tkl = tkl->next;
+    }
+}
