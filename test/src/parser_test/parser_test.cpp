bf4c520d96b9f50f30b0d3dacffd809b3a48ea60
bitquence <imbitquence@gmail.com>
2024-10-03 19:44:04 +0200

    implement pipeline and subshell tests

--- test/src/parser_test/parser_test.cpp
+++ test/src/parser_test/parser_test.cpp
@@ -236,33 +236,259 @@ TEST(ParserIntegration, SimpleAllRedirections) {
 
 // ------------------ PIPELINES ------------------
 
-// cat /etc/passwd | sort
-TEST(ParserIntegration, SimplePipeline) {}
-// cat /etc/passwd | sort | head -n3
-TEST(ParserIntegration, MultiPipeline) {}
-// cat /etc/passwd | < Makefile sort | head -n3 > outfile
-TEST(ParserIntegration, PipelineAndRedirections) {}
-// < infile sort | head > outfile
-TEST(ParserIntegration, PipelineWithTrailingLeadingRedirections) {}
-// < infile sort | grep 'c' | cut -d: | head > outfile
-TEST(ParserIntegration, MultiPipelineWithTrailingLeadingRedirections) {}
+TEST(ParserIntegration, SimplePipeline) {
+	const char *input = "cat /etc/passwd | sort";
+	t_command actual;
+	t_command expected;
+	t_error err;
+
+	err = parse(input, &actual);
+	ASSERT_EQ(err, NO_ERROR);
+
+	expected = command_new_pipeline(
+		command_new_simple(Words({"cat", "/etc/passwd"}).get(), nullptr),
+		command_new_simple(Words({"sort"}).get(), nullptr)
+	);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
+
+TEST(ParserIntegration, MultiPipeline) {
+	const char *input = "cat /etc/passwd | sort | head -n3";
+	t_command actual;
+	t_command expected;
+	t_error err;
+
+	err = parse(input, &actual);
+	ASSERT_EQ(err, NO_ERROR);
+
+	expected = command_new_pipeline(
+		command_new_simple(Words({"cat", "/etc/passwd"}).get(), nullptr),
+		command_new_pipeline(
+			command_new_simple(Words({"sort"}).get(), nullptr),
+			command_new_simple(Words({"head", "-n3"}).get(), nullptr)
+		)
+	);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
+
+TEST(ParserIntegration, PipelineAndRedirections) {
+	const char *input = "cat /etc/passwd | < Makefile sort | head -n3 > outfile";
+	t_command actual;
+	t_command expected;
+	t_error err;
+
+	err = parse(input, &actual);
+	ASSERT_EQ(err, NO_ERROR);
+
+	expected = command_new_pipeline(
+		command_new_simple(Words({"cat", "/etc/passwd"}).get(), nullptr),
+		command_new_pipeline(
+			command_new_simple(Words({"sort"}).get(), Redirections({FromFile("Makefile")}).get()),
+			command_new_simple(Words({"head", "-n3"}).get(), Redirections({IntoFile("outfile")}).get())
+		)
+	);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
+
+TEST(ParserIntegration, PipelineWithTrailingLeadingRedirections) {
+	const char *input = "< infile sort | head -n3 > outfile";
+	t_command actual;
+	t_command expected;
+	t_error err;
+
+	err = parse(input, &actual);
+	ASSERT_EQ(err, NO_ERROR);
+
+	expected = command_new_pipeline(
+		command_new_simple(Words({"sort"}).get(), Redirections({FromFile("infile")}).get()),
+		command_new_simple(Words({"head", "-n3"}).get(), Redirections({IntoFile("outfile")}).get())
+	);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
+
+TEST(ParserIntegration, MultiPipelineWithTrailingLeadingRedirections) {
+	const char *input = "< infile sort | grep c | cut -d: | head > outfile";
+	t_command actual;
+	t_command expected;
+	t_error err;
+
+	err = parse(input, &actual);
+	ASSERT_EQ(err, NO_ERROR);
+
+	expected = command_new_pipeline(
+		command_new_simple(Words({"sort"}).get(), Redirections({FromFile("infile")}).get()),
+		command_new_pipeline(
+			command_new_simple(Words({"grep", "c"}).get(), nullptr),
+			command_new_pipeline(
+				command_new_simple(Words({"cut", "-d:"}).get(), nullptr),
+				command_new_simple(Words({"head"}).get(), Redirections({IntoFile("outfile")}).get())
+			)
+		)
+	);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
 
 // ------------------ SUBSHELLS ------------------
 
-// (echo hello world)
-TEST(ParserIntegration, SimpleSubshell) {}
-// (cat /etc/passwd | sort)
-TEST(ParserIntegration, SubshellWithPipeline) {}
-// (cat /etc/passwd | sort) > outfile
-TEST(ParserIntegration, SubshellWithTrailingRedirection) {}
-// (cat /etc/passwd | sort) > outfile < infile >> appendfile
-TEST(ParserIntegration, SubshellInPipeline) {}
-// ((cat /etc/passwd | grep a) | sort | grep abc)
-TEST(ParserIntegration, SubshellWithTrailingRedirections) {}
-// cat /etc/passwd | (sort | grep abc) | wc
-TEST(ParserIntegration, SimpleNestedSubshellPipeline) {}
-// (a | (b | (c | (d | (e | (f | g))))))
-TEST(ParserIntegration, NestedSubshellPipelines) {}
+TEST(ParserIntegration, SimpleSubshell) {
+	const char *input = "(echo hello world)";
+	t_command actual;
+	t_command expected;
+	t_error err;
+
+	err = parse(input, &actual);
+	ASSERT_EQ(err, NO_ERROR);
+
+	expected = command_new_subshell(
+		command_new_simple(Words({"echo", "hello", "world"}).get(), nullptr),
+		nullptr
+	);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
+
+TEST(ParserIntegration, SubshellWithPipeline) {
+	const char *input = "(cat /etc/passwd | sort)";
+	t_command expected = command_new_subshell(
+		command_new_pipeline(
+			command_new_simple(Words({"cat", "/etc/passwd"}).get(), nullptr),
+			command_new_simple(Words({"sort"}).get(), nullptr)
+		),
+		nullptr
+	);
+
+	t_command actual;
+	ASSERT_EQ(parse(input, &actual), NO_ERROR);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
+
+TEST(ParserIntegration, SubshellWithTrailingRedirection) {
+	const char *input = "(cat /etc/passwd | sort) > outfile";
+	t_command actual;
+	t_command expected;
+	t_error err;
+
+	err = parse(input, &actual);
+	ASSERT_EQ(err, NO_ERROR);
+
+	expected = command_new_subshell(
+		command_new_pipeline(
+			command_new_simple(Words({"cat", "/etc/passwd"}).get(), nullptr),
+			command_new_simple(Words({"sort"}).get(), nullptr)
+		),
+		Redirections({IntoFile("outfile")}).get()
+	);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
+
+TEST(ParserIntegration, SubshellWithAllTrailingRedirections) {
+	const char *input = "(cat /etc/passwd | sort) > outfile < infile >> appendfile";
+	t_command expected = command_new_subshell(
+		command_new_pipeline(
+			command_new_simple(Words({"cat", "/etc/passwd"}).get(), nullptr),
+			command_new_simple(Words({"sort"}).get(), nullptr)
+		),
+		Redirections({IntoFile("outfile"), FromFile("infile"), AppendIntoFile("appendfile")}).get()
+	);
+
+	t_command actual;
+	ASSERT_EQ(parse(input, &actual), NO_ERROR);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
+
+TEST(ParserIntegration, SubshellInPipeline) {
+	const char *input = "cat /etc/passwd | (sort | grep abc) | wc";
+	t_command subshell = command_new_subshell(
+		command_new_pipeline(
+			command_new_simple(Words({"sort"}).get(), nullptr),
+			command_new_simple(Words({"grep", "abc"}).get(), nullptr)
+		),
+		nullptr
+	);
+	t_command expected = command_new_pipeline(
+		command_new_simple(Words({"cat", "/etc/passwd"}).get(), nullptr),
+		command_new_pipeline(
+			subshell,
+			command_new_simple(Words({"wc"}).get(), nullptr)
+		)
+	);
+
+	t_command actual;
+	ASSERT_EQ(parse(input, &actual), NO_ERROR);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
+
+TEST(ParserIntegration, SimpleNestedSubshellPipeline) {
+	const char *input = "((cat /etc/passwd | grep a) | sort | grep abc)";
+	t_command subshell = command_new_subshell(
+		command_new_pipeline(
+			command_new_simple(Words({"cat", "/etc/passwd"}).get(), nullptr),
+			command_new_simple(Words({"grep", "a"}).get(), nullptr)
+		),
+		nullptr
+	);
+	t_command pipeline = command_new_pipeline(
+		subshell,
+		command_new_pipeline(
+			command_new_simple(Words({"sort"}).get(), nullptr),
+			command_new_simple(Words({"grep", "abc"}).get(), nullptr)
+		)
+	);
+	t_command expected = command_new_subshell(pipeline, nullptr);
+
+	t_command actual;
+	ASSERT_EQ(parse(input, &actual), NO_ERROR);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
+
+TEST(ParserIntegration, NestedSubshellPipelines) {
+	const char *input = "(a | (b | (c | (d | (e | (f | g))))))";
+	t_command fg = command_new_subshell(
+		command_new_pipeline(
+			command_new_simple(Words({"f"}).get(), nullptr),
+			command_new_simple(Words({"g"}).get(), nullptr)
+		),
+		nullptr
+	);
+	t_command efg = command_new_subshell(
+		command_new_pipeline(
+			command_new_simple(Words({"e"}).get(), nullptr),
+			fg
+		),
+		nullptr
+	);
+	t_command defg = command_new_subshell(
+		command_new_pipeline(
+			command_new_simple(Words({"d"}).get(), nullptr),
+			efg
+		),
+		nullptr
+	);
+	t_command cdefg = command_new_subshell(
+		command_new_pipeline(
+			command_new_simple(Words({"c"}).get(), nullptr),
+			defg
+		),
+		nullptr
+	);
+	t_command bcdefg = command_new_subshell(
+		command_new_pipeline(
+			command_new_simple(Words({"b"}).get(), nullptr),
+			cdefg
+		),
+		nullptr
+	);
+	t_command expected = command_new_subshell(
+		command_new_pipeline(
+			command_new_simple(Words({"a"}).get(), nullptr),
+			bcdefg
+		),
+		nullptr
+	);
+
+	t_command actual;
+	ASSERT_EQ(parse(input, &actual), NO_ERROR);
+	ASSERT_TRUE(command_eq(actual, expected));
+}
 
 // ------------------ CONDITIONALS ------------------
 
