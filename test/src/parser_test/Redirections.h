e23c65d03d50a4f6df1a3b8e1bf3800fce2eb131
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-03 17:48:53 +0200

    sketch Redirections interface

---
+++ test/src/parser_test/Redirections.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <vector>
+
+extern "C"
+{
+#include "parse/t_command/t_command.h"
+#include "redirection/t_redir_list/t_redir_list.h"
+};
+
+struct Redirections {
+	t_redir_list* self;
+
+	Redirections(const std::vector<const char*>& words);
+	Redirections(const t_simple* simple);
+	Redirections(const t_subshell* subshell);
+	t_word_list* get() const;
+	bool operator==(const Redirections& other) const;
+	bool operator!=(const Redirections& other) const;
+};
+
+t_redirect HereDoc(const char* end);
+t_redirect FromFile(const char* file);
+t_redirect IntoFile(const char* file);
+t_redirect AppendIntoFile(const char* file);
