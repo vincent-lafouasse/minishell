a1d0f41ae3f5cbe8fa022d5b53f940f7a5688d7f
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-04 13:40:43 +0200

    add bad_detector script that detects prototype-only constructs in production code

---
+++ aux/bad_detector.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PATTERNS=(bad dummy hack todo)
+
+for pattern in "${PATTERNS[@]}"; do
+	grep --recursive --ignore-case "${pattern}" ./src/
+done
+
+MAKEFILE_HAS_WERROR=$(grep '^CFLAGS += -Werror' Makefile)
+if [ -z "${MAKEFILE_HAS_WERROR}" ];
+then
+	echo "Makefile doesn't compile with -Werror"
+fi
