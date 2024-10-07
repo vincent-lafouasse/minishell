9f49a0c525d3ffd985fad0706a280bd6fdfe97c7
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-07 19:25:30 +0200

    check for error status of tkl_push_back

--- src/parse/tokenize/tokenize.c
+++ src/parse/tokenize/tokenize.c
@@ -1,13 +1,7 @@
 #include "tokenize.h"
 #include "t_lexer/t_lexer.h"
-#include "log/log.h"
 
-static void cleanup_log(t_lexer *lexer, t_error err);
-
-/*
- * TAKE OOM SERIOUSLY IN PRODUCTION MODE
- * ie do not ignore failability of tkl_push_back
- */
+static t_error cleanup_propagate(t_lexer *lexer, t_error err);
 
 t_error tokenize(const char *input, t_token_list** out)
 {
@@ -22,21 +16,21 @@ t_error tokenize(const char *input, t_token_list** out)
         lexer.start = lexer.current;
         err = lexer_scan_next_token(&lexer, &token);
         if (err != NO_ERROR)
-        {
-            cleanup_log(&lexer, err);
-            return (err);
-        }
+            return cleanup_propagate(&lexer, err);
         err = tkl_push_back(&lexer.tokens, token);
+        if (err != NO_ERROR)
+            return cleanup_propagate(&lexer, err);
         lexer_skip_whitespace(&lexer);
     }
-    tkl_push_back(&lexer.tokens, (t_token){.type = EOF_TOKEN});
+    err = tkl_push_back(&lexer.tokens, (t_token){.type = EOF_TOKEN});
+    if (err != NO_ERROR)
+        return cleanup_propagate(&lexer, err);
     *out = lexer.tokens;
     return (NO_ERROR);
 }
 
-static void cleanup_log(t_lexer *lexer, t_error err)
+static t_error cleanup_propagate(t_lexer *lexer, t_error err)
 {
-    log_token_list(lexer->tokens);
-    log_error(err);
     tkl_clear(&lexer->tokens);
+    return err;
 }
