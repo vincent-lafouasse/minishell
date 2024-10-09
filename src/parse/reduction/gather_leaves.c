84f595e0cf7d555645d7b75043d95fa1335c1cee
bitquence <imbitquence@gmail.com>
2024-10-09 18:42:56 +0200

    make `gather_leaves` deep copy tokens

--- src/parse/reduction/gather_leaves.c
+++ src/parse/reduction/gather_leaves.c
@@ -1,3 +1,4 @@
+#include "parse/tokenize/t_token.h"
 #include "reduction_internals.h"
 #include "../tokenize/t_token_list/t_token_list.h"
 
@@ -5,10 +6,16 @@ static t_error recurse(t_symbol* root, t_token_list** leaves_p)
 {
 	size_t	i;
 	t_error	err;
+	t_token copy;
 
 	i = 0;
-    if (root->kind == TERMINAL)
-        return tkl_push_back(leaves_p, root->token);
+	if (root->kind == TERMINAL)
+	{
+		err = token_duplicate(root->token, &copy);
+		if (err != NO_ERROR)
+			return (err);
+		return tkl_push_back(leaves_p, copy);
+	}
 	while (i < root->production->len)
 	{
 		err = recurse(&root->production->data[i], leaves_p);
