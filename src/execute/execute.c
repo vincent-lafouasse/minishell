ff706646b59b131fc518535b77f9c5abe10d3bd2
bitquence <imbitquence@gmail.com>
2024-10-15 20:07:38 +0200

    extract `apply_redirections` into t_redir_list

--- src/execute/execute.c
+++ src/execute/execute.c
@@ -9,9 +9,7 @@
 #include "word/word.h"
 
 #include <stdio.h>
-#include <fcntl.h>
 #include <stdlib.h>
-#include <unistd.h>
 #include <assert.h> // bad
 
 #define READ 0
@@ -34,50 +32,6 @@
 //            |      signal `must_exit`
 //            |
 //            |----> succeeds
-
-int open_flags_for_redir_kind(t_redir_kind kind)
-{
-	if (kind == FROM_FILE)
-		return O_RDONLY;
-	else if (kind == INTO_FILE)
-		return O_CREAT | O_RDWR | O_TRUNC;
-	else if (kind == APPEND_INTO_FILE)
-		return O_CREAT | O_APPEND | O_RDWR;
-	else
-		return -1;
-}
-
-static t_error apply_redirections(t_redir_list *redirections)
-{
-	t_redirect redir;
-	int fd;
-
-	while (redirections)
-	{
-		redir = redirections->redirect;
-
-		if (redir.kind == HERE_DOCUMENT)
-			return (E_DUMMY);
-
-		/*
-		char *expanded_filename;
-		err = expand(redir.filename, &expanded_filename);
-		if (AMBIGUOUS_REDIRECT)
-			handle();
-		*/
-
-		fd = open(redir.filename, O_CLOEXEC | open_flags_for_redir_kind(redir.kind), 0666);
-		if (fd == -1)
-			return E_DUMMY; // bad, should short circuit command evaluation if file does not exist
-		if (redir.kind == FROM_FILE || redir.kind == HERE_DOCUMENT)
-			dup2(fd, STDIN_FILENO); // bad, should report dup2 error
-		else if (redir.kind == INTO_FILE || redir.kind == APPEND_INTO_FILE)
-			dup2(fd, STDOUT_FILENO); // bad, should report dup2 error
-
-		redirections = redirections->next;
-	}
-	return (NO_ERROR);
-}
 
 _Noreturn
 static void graceful_exit_from_child() // bad dummy
