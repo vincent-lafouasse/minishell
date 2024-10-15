9d2ddfb1c1e37f6d57c28a9f1858aac4c707055b
bitquence <imbitquence@gmail.com>
2024-10-15 15:42:33 +0200

    fix typo in `t_pid_list` header

--- src/execute/t_pid_list/t_pid_list.h
+++ src/execute/t_pid_list/t_pid_list.h
@@ -12,7 +12,7 @@ typedef struct s_pid_list {
 t_pid_list* pidl_new(pid_t pid);
 
 void pidl_push_back_link(t_pid_list** pidl, t_pid_list* link);
-t_pid_list* pop_front_link(t_pid_list** pidl);
+t_pid_list* pidl_pop_front_link(t_pid_list** pidl);
 
 t_error pidl_push_back(t_pid_list** pidl, pid_t pid);
 pid_t pidl_pop_front(t_pid_list** pidl);
