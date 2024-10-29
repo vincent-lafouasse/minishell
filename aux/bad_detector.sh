78c62ea75875ae47f7bd76590ae8d70b2ad32523
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-29 21:29:08 +0100

    register use of GNU find in Makefile as bad (should hardcode sources)

--- aux/bad_detector.sh
+++ aux/bad_detector.sh
@@ -11,3 +11,9 @@ if [ -z "${MAKEFILE_HAS_WERROR}" ];
 then
 	echo "Makefile doesn't compile with -Werror"
 fi
+
+MAKEFILE_HAS_WILDCARD=$(grep find Makefile)
+if [ -n "${MAKEFILE_HAS_WILDCARD}" ];
+then
+	echo "Makefile uses wildcard to list sources"
+fi
