f5e3e3ffb7b417765fff52b1486f9f5530bad2ed
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-10-29 18:29:09 +0100

    sketch [execute,launch]_subshell

--- src/execute/subshell.c
+++ src/execute/subshell.c
@@ -3,6 +3,9 @@
 #include "parse/t_command/t_command.h"
 #include <unistd.h>
 #include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <sys/wait.h>
 
 static void warn_non_empty_redirs(const t_subshell* s) {
 	const char* msg = "minishell: warning: ignored redirs in subshell\n";
@@ -10,7 +13,39 @@ static void warn_non_empty_redirs(const t_subshell* s) {
 		write(STDERR_FILENO, msg, ft_strlen(msg));
 }
 
-static t_launch_result launch_subshell(t_state* state, t_subshell* subshell, t_io io);
+t_launch_result launch_subshell(t_state *state, t_subshell *subshell, t_io io, t_fd_list **fds_to_close) {
+	warn_non_empty_redirs(subshell);
+	t_error err;
+	t_pid_list* pids = pidl_new(0);
+	if (pids == NULL)
+	{
+		free(pids);
+		return (t_launch_result){.error = E_OOM, .pids = NULL};
+	}
+
+	pid_t pid = fork();
+	if (pid == -1)
+	{
+		free(pids);
+		return (t_launch_result){.error = E_FORK, .pids = NULL};
+	}
+	if (pid != 0)
+	{
+		pids->pid = pid;
+		return (t_launch_result){.error = NO_ERROR, .pids = pids};
+	}
+
+	t_command inner = subshell->cmd;
+	assert (inner.type == SIMPLE_CMD);
+
+	err = do_piping(io);
+	if (err != NO_ERROR)
+		perror("dup2");
+
+	t_launch_result launch_res = launch_simple_command(state, inner.simple, io, fds_to_close);
+
+	return launch_res;
+}
 
 t_command_result execute_subshell(t_state *state, t_subshell *subshell)
 {
@@ -18,5 +53,15 @@ t_command_result execute_subshell(t_state *state, t_subshell *subshell)
 	t_command inner = subshell->cmd;
 	assert (inner.type == SIMPLE_CMD);
 
-	//t_launch_result launch_result = launch_subshell(state, subshell);
+	t_launch_result launch_result = launch_subshell(state, subshell, io_default(), NULL);
+	if (launch_result.error != NO_ERROR) {
+		return (t_command_result){.error = launch_result.error};
+	}
+	pid_t to_wait = launch_result.pids->pid;
+
+	int status;
+	int options = 0;
+	waitpid(to_wait, &status, options);
+
+	return (t_command_result){.error = NO_ERROR, .status_code = status};
 }
