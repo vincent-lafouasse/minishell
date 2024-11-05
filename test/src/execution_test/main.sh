c6694d006e11ab916ed49ffbbbce17f818c37929
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-05 13:40:28 +0100

    format

--- test/src/execution_test/main.sh
+++ test/src/execution_test/main.sh
@@ -2,23 +2,22 @@ root_dir=~/code/42/cc/minishell
 minishell="${root_dir}/minishell"
 src_dir="${root_dir}/test/src/execution_test"
 
-setup () {
-	if make -C "$root_dir"; then
-		echo minishell build ok
-	else
-		echo Error: couldnt build minishell
-		exit 1
-	fi
+setup() {
+    if make -C "$root_dir"; then
+        echo minishell build ok
+    else
+        echo Error: couldnt build minishell
+        exit 1
+    fi
 }
 
-
-test () {
-	input="$1"
+test() {
+    input="$1"
 }
 
-main () {
-	setup
-	test "echo hello"
+main() {
+    setup
+    test "echo hello"
 }
 
 main
