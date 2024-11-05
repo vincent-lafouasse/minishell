f4e76aea2c564069c49521b0691ad8dbc2c2e7ac
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-05 13:37:12 +0100

    sketch

--- test/src/execution_test/main.sh
+++ test/src/execution_test/main.sh
@@ -1,4 +1,5 @@
 root_dir=~/code/42/cc/minishell
+minishell="${root_dir}/minishell"
 src_dir="${root_dir}/test/src/execution_test"
 
 setup () {
@@ -8,6 +9,11 @@ setup () {
 		echo Error: couldnt build minishell
 		exit 1
 	fi
+}
+
+
+test () {
+	input="$1"
 }
 
 main () {
