8ae3110678c000fc7dbf83cf3af7715e086f0f03
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-10 16:54:40 +0100

    sketch

--- test/src/execution_test/main.py
+++ test/src/execution_test/main.py
@@ -1,3 +1,12 @@
 import subprocess
 
+
+class CommandRunner:
+    def __init__(self, command: str):
+        pass
+
+    def run(self) -> subprocess.CompletedProcess:
+        pass
+
+
 subprocess.run("ls -la", shell=True)
