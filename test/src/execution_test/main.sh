190558905dfaad769ccf44427ebd84244129256a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-11 12:24:22 +0100

    refactor

--- test/src/execution_test/main.sh
+++ test/src/execution_test/main.sh
@@ -12,16 +12,23 @@ setup() {
     mkdir -p "$BUILD"
 }
 
-test_command() {
-    command="$1"
-    test_name="$2"
-
-    infile_dir="${EXEC_TEST_ROOT}/${test_name}"
+setup_test() {
+    test_name="$1"
     build="${BUILD}/${test_name}"
     bash_output="${build}/bash"
     minishell_output="${build}/minishell"
     mkdir -p "${bash_output}"
     mkdir -p "${minishell_output}"
+}
+
+test_command() {
+    command="$1"
+    test_name="$2"
+    setup_test "$test_name"
+
+    infile_dir="${EXEC_TEST_ROOT}/${test_name}"
+    bash_output="${BUILD}/${test_name}/bash"
+    minishell_output="${BUILD}/${test_name}/minishell"
 
     $MINISHELL -c "$command" >"${minishell_output}/stdout" 2>"${minishell_output}/stderr"
     minishell_status=$?
