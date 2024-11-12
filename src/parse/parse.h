389cc70aa16f6fcdaba16af4cfc6b38b3c2c02fb
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-12 13:43:59 +0100

    format

--- src/parse/parse.h
+++ src/parse/parse.h
@@ -1,6 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 13:43:48 by poss              #+#    #+#             */
+/*   Updated: 2024/11/12 13:43:49 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
 #include "error/t_error.h"
 #include "t_command/t_command.h"
 
-t_error parse(const char *input, t_command *out);
+t_error	parse(const char *input, t_command *out);
