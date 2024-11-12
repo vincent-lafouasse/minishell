bec4b2950725e80740e7e073a6b929d4ba7fce6a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-12 13:50:27 +0100

    format

--- src/parse/tokenize/t_lexer/lexer_internals.h
+++ src/parse/tokenize/t_lexer/lexer_internals.h
@@ -1,15 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lexer_internals.h                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 13:46:02 by poss              #+#    #+#             */
+/*   Updated: 2024/11/12 13:48:04 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
 #include "t_lexer.h"
 
 #define METACHARACTERS "()<>|&"
 #define WHITESPACE " \t\n"
-#define SHELL_BREAK_CHARS METACHARACTERS WHITESPACE
+#define SHELL_BREAK_CHARS " \t\n()<>|&"
 
-t_error lexer_scan_metacharacter_token(t_lexer *lexer, t_token *out);
-t_error lexer_scan_word(t_lexer *lexer, t_token *out);
+t_error	lexer_scan_metacharacter_token(t_lexer *lexer, t_token *out);
+t_error	lexer_scan_word(t_lexer *lexer, t_token *out);
 
-char lexer_advance(t_lexer *lexer);
-char lexer_peek(t_lexer *lexer);
+char	lexer_advance(t_lexer *lexer);
+char	lexer_peek(t_lexer *lexer);
 
-t_error fill_token(t_token token, t_token *out);
+t_error	fill_token(t_token token, t_token *out);
--- src/parse/tokenize/t_lexer/t_lexer.h
+++ src/parse/tokenize/t_lexer/t_lexer.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_lexer.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 13:46:14 by poss              #+#    #+#             */
+/*   Updated: 2024/11/12 13:46:25 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
 #include "../t_token_list/t_token_list.h"
@@ -6,13 +18,13 @@
 
 typedef struct s_lexer
 {
-    const char *source;
-    t_token_list *tokens;
-    size_t src_len;
-    size_t current;
-    size_t start;
-} t_lexer;
+	const char		*source;
+	t_token_list	*tokens;
+	size_t			src_len;
+	size_t			current;
+	size_t			start;
+}					t_lexer;
 
-t_lexer lexer_init(const char *source);
-t_error lexer_scan_next_token(t_lexer *lexer, t_token *out);
-void lexer_skip_whitespace(t_lexer *lexer);
+t_lexer				lexer_init(const char *source);
+t_error				lexer_scan_next_token(t_lexer *lexer, t_token *out);
+void				lexer_skip_whitespace(t_lexer *lexer);
