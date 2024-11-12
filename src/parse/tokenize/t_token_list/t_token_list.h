e738f332a504bf9cb0c88ac7c8ebf13bb898f50e
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-12 13:51:59 +0100

    format

--- src/parse/tokenize/t_token_list/t_token_list.h
+++ src/parse/tokenize/t_token_list/t_token_list.h
@@ -1,19 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_token_list.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/11/12 13:51:07 by poss              #+#    #+#             */
+/*   Updated: 2024/11/12 13:51:08 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
 #include "../t_token.h"
 #include "error/t_error.h"
 
-typedef void (*t_destructor)(void *);
+typedef void			(*t_destructor)(void *);
 
 typedef struct s_token_list
 {
-    t_token token;
-    struct s_token_list *next;
-    struct s_token_list *prev;
-} t_token_list;
+	t_token				token;
+	struct s_token_list	*next;
+	struct s_token_list	*prev;
+}						t_token_list;
 
-t_token_list *tkl_new(t_token token);
-void tkl_push_back_link(t_token_list **tokens_p, t_token_list *link);
-t_error tkl_push_back(t_token_list **tokens_p, t_token token);
-void tkl_delone(t_token_list **tokens_p, t_destructor literal_destructor);
-void tkl_clear(t_token_list **tokens_p, t_destructor literal_destructor);
+t_token_list			*tkl_new(t_token token);
+void					tkl_push_back_link(t_token_list **tokens_p,
+							t_token_list *link);
+t_error					tkl_push_back(t_token_list **tokens_p, t_token token);
+void					tkl_delone(t_token_list **tokens_p,
+							t_destructor literal_destructor);
+void					tkl_clear(t_token_list **tokens_p,
+							t_destructor literal_destructor);
