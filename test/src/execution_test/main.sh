620216793927ad1de38f680a2f6caecd2ee34af0
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-12 17:14:47 +0100

    mark local variables as local

--- test/src/execution_test/main.sh
+++ test/src/execution_test/main.sh
@@ -15,21 +15,21 @@ setup() {
 }
 
 setup_test() {
-    test_name="$1"
+    local test_name="$1"
 
-    build="${BUILD}/${test_name}"
-    bash_output="${build}/bash"
-    minishell_output="${build}/minishell"
+    local build="${BUILD}/${test_name}"
+    local bash_output="${build}/bash"
+    local minishell_output="${build}/minishell"
     mkdir -p "${bash_output}"
     mkdir -p "${minishell_output}"
 }
 
 # wip
 compare_outputs() {
-    test_name=$1
+    local test_name=$1
 
-    bash_output="${BUILD}/${test_name}/bash"
-    minishell_output="${BUILD}/${test_name}/minishell"
+    local bash_output="${BUILD}/${test_name}/bash"
+    local minishell_output="${BUILD}/${test_name}/minishell"
 
     for file in "$bash_output"/*; do
         DIFF="$(diff "$file")"
@@ -37,26 +37,23 @@ compare_outputs() {
 }
 
 compare_with_bash() {
-    test_name="$1"
-    command="$2"
+    local test_name="$1"
+    local command="$2"
 
     setup_test "$test_name"
 
-    bash_output="${BUILD}/${test_name}/bash"
-    minishell_output="${BUILD}/${test_name}/minishell"
+    local bash_output="${BUILD}/${test_name}/bash"
+    local minishell_output="${BUILD}/${test_name}/minishell"
 
-    command="${command//INFILE_DIR/${INFILE_DIR}}"
-    minishell_command="${command//OUTFILE_DIR/${minishell_output}}"
-    bash_command="${command//OUTFILE_DIR/${bash_output}}"
+    local command="${command//INFILE_DIR/${INFILE_DIR}}"
+    local minishell_command="${command//OUTFILE_DIR/${minishell_output}}"
+    local bash_command="${command//OUTFILE_DIR/${bash_output}}"
 
     $MINISHELL -c "$minishell_command" >"${minishell_output}/stdout" 2>"${minishell_output}/stderr"
-    minishell_status=$?
+    echo $? > "${minishell_output}/status"
 
     bash -c "$bash_command" >"${bash_output}/stdout" 2>"${bash_output}/stderr"
-    bash_status=$?
-
-    echo bash status "$bash_status"
-    echo mini status "$minishell_status"
+    echo $? > "${bash_output}/status"
 
     diff --brief "$minishell_output" "$bash_output"
 }
