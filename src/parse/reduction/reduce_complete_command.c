a1607c9745fe046e6502ec78f62897b0a9b2e3a9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-12 19:48:12 +0100

    rename operator to op to avoid shenanigans

--- src/parse/reduction/reduce_complete_command.c
+++ src/parse/reduction/reduce_complete_command.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 14:25:41 by poss              #+#    #+#             */
-/*   Updated: 2024/11/12 14:25:44 by poss             ###   ########.fr       */
+/*   Updated: 2024/11/12 19:47:58 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,12 +14,9 @@
 #include "../t_symbol/t_symbol.h"
 #include "../tokenize/t_token.h"
 #include "error/t_error.h"
-#include "libft/stdlib.h"
 #include "reduction_internals.h"
 #include <assert.h> // temporarily
 #include <stdbool.h>
-#include <stdio.h> // temporarily
-#include <stdlib.h>
 
 static t_conditional_operator	operator_from_token_type(t_token_type type)
 {
@@ -35,7 +32,7 @@ static t_error	reduce_complete_cmd_rest(t_symbol *cmd_rest, t_command *out)
 {
 	t_symbol_array			*productions;
 	t_symbol				next_cmd_rest;
-	t_conditional_operator	operator;
+	t_conditional_operator	op;
 	t_error					err;
 
 	assert(cmd_rest->kind == SYM_COMPLETE_CMD_REST);
@@ -43,8 +40,9 @@ static t_error	reduce_complete_cmd_rest(t_symbol *cmd_rest, t_command *out)
 	next_cmd_rest = productions->data[2];
 	if (next_cmd_rest.production->len == 0)
 		return (reduce_pipeline(&productions->data[1], out));
-	operator= operator_from_token_type(next_cmd_rest.production->data[0].token.type);
-	*out = command_new_conditional(operator,(t_command){0}, (t_command){0});
+	op = operator_from_token_type(
+			next_cmd_rest.production->data[0].token.type);
+	*out = command_new_conditional(op, (t_command){0}, (t_command){0});
 	if (!command_is_initialized(*out))
 		return (E_OOM);
 	err = reduce_pipeline(&productions->data[1], &out->conditional->first);
