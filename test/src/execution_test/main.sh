a6e97fe0c22afce10393b00b760e78f75eb66686
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-14 13:21:15 +0100

    count number of failed tests

--- test/src/execution_test/main.sh
+++ test/src/execution_test/main.sh
@@ -24,18 +24,6 @@ setup_test() {
     mkdir -p "${minishell_output}"
 }
 
-# wip
-compare_outputs() {
-    local test_name=$1
-
-    local bash_output="${BUILD}/${test_name}/bash"
-    local minishell_output="${BUILD}/${test_name}/minishell"
-
-    for file in "$bash_output"/*; do
-        DIFF="$(diff "$file")"
-    done
-}
-
 compare_with_bash() {
     local test_name="$1"
     local command="$2"
@@ -55,20 +43,27 @@ compare_with_bash() {
     bash -c "$bash_command" >"${bash_output}/stdout" 2>"${bash_output}/stderr"
     echo $? >"${bash_output}/status"
 
-    if ! diff --brief "$minishell_output" "$bash_output"; then
-        HAD_ERROR=1
+    if diff "$minishell_output" "$bash_output" >"${BUILD}/${test_name}/log"; then
+        ((N_PASSED++))
+    else
+        ((N_FAILED++))
     fi
 }
 
 main() {
-    HAD_ERROR=0
+    N_PASSED=0
+    N_FAILED=0
 
-    compare_with_bash 'HelloWorld'       'echo hello world'
-    compare_with_bash 'CanTakeInfile'    'cat INFILE_DIR/Makefile'
-    compare_with_bash 'SeparateOutfiles' 'echo 420 > OUTFILE_DIR/out'
-    compare_with_bash 'EnvStuff'         'export COOL_NUMBER=420; echo $COOL_NUMBER'
+    compare_with_bash 'HelloWorld'          'echo hello world'
+    compare_with_bash 'CanTakeInfile'       'cat INFILE_DIR/Makefile'
+    compare_with_bash 'SeparateOutfiles'    'echo 420 > OUTFILE_DIR/out'
+    compare_with_bash 'EnvStuff'            'export COOL_NUMBER=420; echo $COOL_NUMBER'
 
-    exit "$HAD_ERROR"
+    if [ "$N_FAILED" -eq 0 ]; then
+        exit 0
+    else
+        exit 1
+    fi
 }
 
 main
