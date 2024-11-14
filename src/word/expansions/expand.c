7f3abfbbe1179b1311071f0c6fd36e75e58bfd7d
bitquence <imbitquence@gmail.com>
2024-11-14 13:56:26 +0100

    replace `quote_context` enum for bool

--- src/word/expansions/expand.c
+++ src/word/expansions/expand.c
@@ -40,28 +40,18 @@ static bool identifier_len(const char *str) {
 	return (len);
 }
 
-enum e_quote_context {NOT_IN_QUOTE, IN_SINGLE_QUOTE, IN_DOUBLE_QUOTE};
-
 t_error expand_single_word(t_expansion_variables vars, char* filename)
 {
-	enum e_quote_context quote_ctx = NOT_IN_QUOTE;
+	bool should_be_expanding;
+	size_t i;
 
-	size_t i = 0;
+	i = 0;
+	should_be_expanding = false;
 	while (filename[i]) {
-		if (filename[i] == '\'') {
-			while (filename[i] && filename[i] != '\'')
-				i++;
-		}
+		if (filename[i] == '\'')
+			should_be_expanding = !should_be_expanding;
 
-		if (filename[i] == '"') {
-			if (quote_ctx == NOT_IN_QUOTE) {
-				quote_ctx = IN_DOUBLE_QUOTE;
-			} else if (quote_ctx == IN_DOUBLE_QUOTE) {
-				quote_ctx = NOT_IN_QUOTE;
-			}
-		}
-
-		if (filename[i] == '$') {
+		if (should_be_expanding && filename[i] == '$') {
 			// xxx make sure nothing breaks if variable_name_len = 0
 			size_t variable_name_len = identifier_len(filename + i);
 			if (variable_name_len != 0)
