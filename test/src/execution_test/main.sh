af54ba5faf8988c1bd7c5e565fed7e026583fc59
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-14 16:19:45 +0100

    more tests

--- test/src/execution_test/main.sh
+++ test/src/execution_test/main.sh
@@ -58,19 +58,61 @@ compare_with_bash() {
     fi
 }
 
+refute() {
+    local test_name="$1"
+    local command="$2"
+    local status="$3"
+    local partial_stderr="$4"
+
+    local build_dir="${BUILD}/${test_name}"
+    mkdir -p "$build_dir"
+    echo -e "${YELLOW}Testing $test_name$NC"
+
+    local command="${command//INFILE_DIR/${INFILE_DIR}}"
+    local command="${command//OUTFILE_DIR/${build_dir}}"
+
+    $MINISHELL -c "$minishell_command" >"${build_dir}/stdout" 2>"${build_dir}/stderr"
+    local actual_status=$?
+
+    local had_error=0
+
+    if [ "$actual_status" -ne "$status" ]; then
+        echo -e "    ${RED}Expected status $status was $actual_status$NC"
+        had_error=1
+    fi
+
+    if [ -z "$partial_stderr" ]; then
+        if ! grep -q "${build_dir}/stderr" "$partial_stderr"; then
+            echo -e "    ${RED}Stderr doesnt contain pattern \'$partial_stderr\'$NC"
+            had_error=1
+        fi
+    fi
+
+    if [ "$had_error" -eq 0 ]; then
+        echo -e "${GREEN}✓   ${test_name} passed${NC}"
+        ((N_PASSED++))
+    else
+        echo -e "${RED}✗   ${test_name} failed${NC}"
+        ((N_FAILED++))
+        FAILED_TESTS+=("${test_name}")
+    fi
+}
+
 main() {
     N_PASSED=0
     N_FAILED=0
     FAILED_TESTS=()
     setup
 
-    compare_with_bash 'Simple_HelloWorld'   'echo hello world'
-    compare_with_bash 'Simple_pwdIntoFile'  'pwd > OUTFILE_DIR/pwd_log'
-    compare_with_bash 'Simple_SearchFile'   'grep thou INFILE_DIR/shakespeare.txt'
+    compare_with_bash 'Simple_HelloWorld'  'echo hello world'
+    compare_with_bash 'Simple_pwdIntoFile' 'pwd > OUTFILE_DIR/pwd_log'
+    compare_with_bash 'Simple_SearchFile'  'grep thou INFILE_DIR/shakespeare.txt'
 
-    compare_with_bash 'Pipe_FindIncludes'   'grep -r include ./src | sort | uniq'
+    compare_with_bash 'Pipe_FindIncludes' 'grep -r include ./src | sort | uniq'
 
-    compare_with_bash 'EnvStuff'            'export COOL_NUMBER=420 && echo $COOL_NUMBER'
+    compare_with_bash 'EnvStuff' 'export COOL_NUMBER=420 && echo $COOL_NUMBER'
+
+    refute 'Refute_NonExistantCommand' 'man_i_sure_hope_this_command_doesnt_exist' 127 'command not found'
 
     if [ "$N_FAILED" -eq 0 ]; then
         echo -e "\n${GREEN}==========ALL TESTS PASSED======================================================${NC}"
