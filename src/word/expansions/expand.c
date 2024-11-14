f5c2a05cc485b69d1fe4206e399cb6badb72fbdf
bitquence <imbitquence@gmail.com>
2024-11-14 16:29:02 +0100

    implement last status expansion and seperate code

--- src/word/expansions/expand.c
+++ src/word/expansions/expand.c
@@ -1,11 +1,13 @@
 #include "expand.h"
 #include "execute/t_env/t_env.h"
 #include "error/t_error.h"
+#include "libft/string.h"
 #include "word/t_string/t_string.h"
 
 #include "libft/ctype.h"
 #include "libft/ft_string.h"
 
+#include <stdlib.h>
 #include <stdbool.h>
 
 t_error perform_all_expansions_on_words(t_word_list *word)
@@ -47,6 +49,41 @@ static bool can_expand_variable(const char *substring)
 	return (*substring == '$' && identifier_len(substring + 1) > 0);
 }
 
+
+static t_error expand_dollar_variable(t_expansion_variables vars, \
+									  const char *start, t_string **out)
+{
+	char *variable_name;
+	const char *after_dollar;
+	const t_env_entry *variable;
+	char *replace_with;
+	t_error err;
+
+	after_dollar = start + 1;
+	variable_name = ft_substr(after_dollar, 0, identifier_len(after_dollar));
+	if (!variable_name)
+		return (E_OOM);
+	if (*after_dollar == '?')
+		replace_with = ft_itoa(vars.last_status);
+	else
+	{
+		variable = env_get(vars.env, variable_name);
+		if (variable)
+			replace_with = ft_strdup(variable->value);
+		else
+			replace_with = ft_strdup("");
+	}
+	if (!replace_with)
+	{
+		free(variable_name);
+		return (E_OOM);
+	}
+	err = string_extend(out, replace_with);
+	free(variable_name);
+	free(replace_with);
+	return (err);
+}
+
 t_error variable_expand_string(t_expansion_variables vars, const char *str, char **out)
 {
 	bool should_be_expanding;
@@ -60,20 +97,12 @@ t_error variable_expand_string(t_expansion_variables vars, const char *str, char
 	while (str[i]) {
 		if (str[i] == '\'')
 			should_be_expanding = !should_be_expanding;
-		if (should_be_expanding && can_expand_variable(&str[i])) {
-			size_t variable_name_len = identifier_len(str + i + 1);
-			char *variable_name = ft_substr(str, i + 1, variable_name_len);
-			if (!variable_name)
+		if (should_be_expanding && can_expand_variable(&str[i]))
+		{
+			err = expand_dollar_variable(vars, &str[i], &expanded);
+			if (err != NO_ERROR)
 				return (string_destroy(expanded), E_OOM);
-			if (str[i + 1] == '?')
-				"expand with last_status";
-			const t_env_entry *variable = env_get(vars.env, variable_name);
-			if (variable)
-			{
-				if (string_extend(&expanded, variable->value) == true)
-					return (string_destroy(expanded), E_OOM);
-			}
-			i += 1 + variable_name_len;
+			i += 1 + identifier_len(&str[i]);
 		}
 		else
 		{
