b56dae86f302717772eba2d3db7797a505d6fe51
bitquence <imbitquence@gmail.com>
2024-11-16 02:32:45 +0100

    sketch join word quotes list join tests

---
+++ test/src/expansion_test/join.cpp
@@ -0,0 +1,11 @@
+#include "gtest/gtest.h"
+
+extern "C"
+{
+#include "word/expansions/t_word_quotes_list/t_word_quotes_list.h"
+};
+
+TEST(WordQuotesListJoin, JoinsSingleUnquotedString)
+{
+    FAIL() << "unimplemented";
+}
