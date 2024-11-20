58454f66b83ddfe2d050b73ffc6cfff28b1287aa
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-11-20 18:37:55 +0100

    dummy implementation of builtins

--- src/execute/builtin.c
+++ src/execute/builtin.c
@@ -1,7 +1,67 @@
 #include "builtin.h"
+#include "error/t_error.h"
+#include "execute.h"
 #include "libft/string.h"
 
+#include <assert.h> // bad
+#include <stdio.h> // bad
+
 static bool str_eq(const char* a, const char* b);
+
+t_command_result execute_export(t_state *state, t_simple *builtin)
+{
+	printf("called export\n");
+	return (t_command_result){.error = E_DUMMY};
+}
+t_command_result execute_echo(t_state *state, t_simple *builtin)
+{
+	printf("called echo\n");
+	return (t_command_result){.error = E_DUMMY};
+}
+
+t_command_result execute_cd(t_state *state, t_simple *builtin)
+{
+	printf("called cd\n");
+	return (t_command_result){.error = E_DUMMY};
+}
+t_command_result execute_unset(t_state *state, t_simple *builtin)
+{
+	printf("called unset\n");
+	return (t_command_result){.error = E_DUMMY};
+}
+t_command_result execute_env(t_state *state, t_simple *builtin)
+{
+	printf("called env\n");
+	return (t_command_result){.error = E_DUMMY};
+}
+t_command_result execute_exit(t_state *state, t_simple *builtin)
+{
+	printf("called exit\n");
+	return (t_command_result){.error = E_DUMMY};
+}
+
+t_command_result execute_builtin(t_state *state, t_simple *simple) {
+	assert(is_builtin_command(simple));
+	assert(simple->words != NULL);
+
+
+	const char* command = simple->words->contents;
+
+	if (str_eq(command, "echo"))
+		return execute_echo(state, simple);
+	if (str_eq(command, "cd"))
+		return execute_cd(state, simple);
+	if (str_eq(command, "export"))
+		return execute_export(state, simple);
+	if (str_eq(command, "unset"))
+		return execute_unset(state, simple);
+	if (str_eq(command, "env"))
+		return execute_env(state, simple);
+	if (str_eq(command, "exit"))
+		return execute_exit(state, simple);
+
+	return (t_command_result){.error = E_UNREACHABLE};
+}
 
 bool is_builtin_command(const t_simple* simple) {
 	if (simple->words == NULL)
