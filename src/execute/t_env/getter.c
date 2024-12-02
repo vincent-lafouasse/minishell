022fba6a219e064bbf59c8148fb5cf9a96ad4f12
bitquence <imbitquence@gmail.com>
2024-12-02 23:01:40 +0100

    implement env_get_mut method

--- src/execute/t_env/getter.c
+++ src/execute/t_env/getter.c
@@ -16,6 +16,16 @@ const t_env_entry	*env_get(const t_env *env, const char *key)
 	return (&node->entry);
 }
 
+t_env_entry	*env_get_mut(t_env *env, const char *key)
+{
+	t_env *node;
+
+	node = env_find_node((t_env*)env, key);
+	if (!node)
+		return NULL;
+	return (&node->entry);
+}
+
 bool	env_key_exists(const t_env *env, const char *key)
 {
 	return env_get(env, key) != NULL;
