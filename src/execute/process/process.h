185f843135fb1027071e157bac0c415351e9ad33
Jonathan <imbitquence@gmail.com>
2024-12-18 17:01:24 +0100

    sketch process module

---
+++ src/execute/process/process.h
@@ -0,0 +1,14 @@
+#ifndef PROCESS_H
+# define PROCESS_H
+
+#include <sys/types.h>
+
+#include "../t_pid_list/t_pid_list.h"
+#include "error/t_error.h"
+
+t_error fork_process(bool *is_inside_of_child, int *pid_out);
+
+t_error wait_for_process(pid_t pid, int *exit_status_out);
+t_error wait_for_pipeline(t_pid_list *pids, int *last_exit_status_out);
+
+#endif // PROCESS_H
