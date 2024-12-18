31757fc63aae42f9bda883de80f154c3b1e1c817
Jonathan <imbitquence@gmail.com>
2024-12-18 18:18:54 +0100

    handle `wait` unwarranted process exit

--- src/execute/process/wait_through_signals.c
+++ src/execute/process/wait_through_signals.c
@@ -14,10 +14,19 @@ pid_t wait_through_signals(pid_t pid, int *status_out)
 		waited_for_pid = waitpid(pid, status_out, 0);
 		if (waited_for_pid < 0 && errno == EINTR)
 			continue;
-		// an event other than the process exiting occured, we don't care for it
-		if (waited_for_pid >= 0 && \
-			(!WIFEXITED(*status_out) && !WIFSIGNALED(*status_out)))
-			continue;
+		if (waited_for_pid >= 0)
+		{
+			// an event other than a process exiting occured, we don't care for it
+			if ((!WIFEXITED(*status_out) && !WIFSIGNALED(*status_out)))
+				continue;
+			// we have caught the termination of a different process than the
+			// one we were looking for; bash would save this to its global job
+			// list to keep track of it but since we can't make global
+			// variables, we have to be careful not to leave any processes
+			// unaccounted for
+			if (pid != -1 && waited_for_pid != pid)
+				continue;
+		}
 		break;
 	}
 	return (waited_for_pid);
