e5c06ee0b738c2c8a49aeb1a411e74b2c5c8f804
Jonathan <imbitquence@gmail.com>
2024-12-23 19:28:33 +0100

    report status code 2 for unrecognized token

--- src/error/parse_error_exit_code.c
+++ src/error/parse_error_exit_code.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 char parse_error_exit_code(t_error err) {
-    if (err == E_UNEXPECTED_TOKEN)
+    if (err == E_UNEXPECTED_TOKEN || err == E_UNRECOGNIZED_TOKEN)
         return 2;
     return EXIT_FAILURE;
 }
