e725040551913a8809eb5b6fe7aa070e3b0c7503
Jonathan <imbitquence@gmail.com>
2024-12-28 17:50:57 +0100

    implement `kill_pipeline` and `kill_process`

---
+++ src/execute/process/kill.c
@@ -0,0 +1,48 @@
+#include "process.h"
+
+#include <stdio.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <unistd.h>
+
+void	attempt_to_reap_processes()
+{
+	int status;
+
+	while (1)
+	{
+		status = waitpid(-1, NULL, WNOHANG);
+		if (status == -1 && errno != EINTR)
+			break;
+	}
+	if (status == -1 && errno != ECHILD && errno != EINTR)
+		perror("minishell: kill_pipeline: waitpid");
+}
+
+void kill_pid_list(const t_pid_list *pids)
+{
+	const t_pid_list *curr;
+
+	curr = pids;
+	while (curr)
+	{
+		kill(curr->pid, SIGKILL);
+		curr = curr->next;
+	}
+}
+
+void	kill_pipeline(t_state *state, const t_pid_list *pids)
+{
+	attempt_to_reap_processes();
+	kill_pid_list(pids);
+	if (state->is_interactive)
+		reset_tty_properties(STDERR_FILENO, &state->tty_properties);
+}
+
+void	kill_process(t_state *state, pid_t pid)
+{
+	attempt_to_reap_processes();
+	kill(pid, SIGKILL);
+	if (state->is_interactive)
+		reset_tty_properties(STDERR_FILENO, &state->tty_properties);
+}
