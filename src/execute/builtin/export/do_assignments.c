daa07cc49f6339d964161d8f3d9c3d9e34d0cf2b
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-22 17:14:51 +0100

    extract do_assignments inner

--- src/execute/builtin/export/do_assignments.c
+++ src/execute/builtin/export/do_assignments.c
@@ -6,15 +6,15 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 16:48:20 by poss              #+#    #+#             */
-/*   Updated: 2025/01/22 16:54:35 by poss             ###   ########.fr       */
+/*   Updated: 2025/01/22 17:14:34 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../builtin.h"
-#include "export_internals.h"
 #include "error/t_error.h"
 #include "execute/execute.h"
 #include "execute/t_env/t_env.h"
+#include "export_internals.h"
 #include "libft/ft_string.h"
 #include "libft/string.h"
 #include <assert.h> // temporarily
@@ -24,9 +24,9 @@
 
 typedef struct s_assignment
 {
-	bool	appending;
-	char	*name;
-	char	*value;
+	bool			appending;
+	char			*name;
+	char			*value;
 }					t_assignment;
 
 static t_error	parse_assignment(char *str, t_assignment *out)
@@ -95,34 +95,47 @@ static t_error	assign_variable(t_env **env, t_assignment assignment)
 	return (NO_ERROR);
 }
 
-t_command_result	do_assignments(t_env **env, t_word_list *assignments)
+static t_error	process_assignment(char *s, t_env **env, bool *any_failed)
 {
-	bool			any_failed;
 	t_assignment	assignment;
 	t_error			err;
+
+	err = parse_assignment(s, &assignment);
+	if (err != NO_ERROR)
+		return (err);
+	if (!assignment.name)
+	{
+		*any_failed = true;
+		report_invalid_identifier(s);
+		return (NO_ERROR);
+	}
+	err = assign_variable(env, assignment);
+	if (err != NO_ERROR)
+	{
+		free(assignment.name);
+		free(assignment.value);
+		return (err);
+	}
+	return (NO_ERROR);
+}
+
+t_command_result	do_assignments(t_env **env, t_word_list *assignments)
+{
+	bool	any_failed;
+	t_error	err;
 
 	any_failed = false;
 	while (assignments)
 	{
-		err = parse_assignment(assignments->contents, &assignment);
+		err = process_assignment(assignments->contents, env, &any_failed);
 		if (err != NO_ERROR)
 			return ((t_command_result){.error = err});
-		if (!assignment.name)
-		{
-			any_failed = true;
-			report_invalid_identifier(assignments->contents);
-			assignments = assignments->next;
-			continue ;
-		}
-		err = assign_variable(env, assignment);
-		if (err != NO_ERROR)
-		{
-			free(assignment.name);
-			free(assignment.value);
-			return ((t_command_result){.error = err});
-		}
 		assignments = assignments->next;
 	}
-	return ((t_command_result){.error = NO_ERROR,
-		.status_code = any_failed ? EXIT_FAILURE : EXIT_SUCCESS});
+	if (any_failed)
+		return ((t_command_result){.error = NO_ERROR,
+			.status_code = EXIT_FAILURE});
+	else
+		return ((t_command_result){.error = NO_ERROR,
+			.status_code = EXIT_SUCCESS});
 }
