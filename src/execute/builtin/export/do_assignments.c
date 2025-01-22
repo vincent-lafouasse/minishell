7942a4817dd078a9c1deb7f1e46b887776d5d678
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-22 16:51:36 +0100

    format

--- src/execute/builtin/export/do_assignments.c
+++ src/execute/builtin/export/do_assignments.c
@@ -1,37 +1,46 @@
-#include "../builtin.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   do_assignments.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 16:48:20 by poss              #+#    #+#             */
+/*   Updated: 2025/01/22 16:51:31 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "../builtin.h"
+#include "error/t_error.h"
 #include "execute/execute.h"
 #include "execute/t_env/t_env.h"
-
-#include "error/t_error.h"
-#include "libft/string.h"
-#include "libft/ft_string.h"
 #include "libft/ctype.h"
-
+#include "libft/ft_string.h"
+#include "libft/string.h"
+#include <assert.h> // temporarily
 #include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#include <assert.h> // temporarily
+#define NOT_A_VALID_ID "minishell: export: not a valid identifier\n"
 
 typedef struct s_assignment
 {
-	bool appending;
-	char *name;
-	char *value;
-} t_assignment;
+	bool	appending;
+	char	*name;
+	char	*value;
+}					t_assignment;
 
-static void report_invalid_identifier(char *identifier_name)
+static void	report_invalid_identifier(char *identifier_name)
 {
 	(void)identifier_name;
-	const char *message = "minishell: export: not a valid identifier\n";
-	write(STDERR_FILENO, message, ft_strlen(message));
+	write(STDERR_FILENO, NOT_A_VALID_ID, ft_strlen(NOT_A_VALID_ID));
 }
 
 // returns 0 if assignment is malformated in any way
-static size_t name_len(char *assignment)
+static size_t	name_len(char *assignment)
 {
-	size_t i;
+	size_t	i;
 
 	if (!ft_isalpha(*assignment) && *assignment != '_')
 		return (0);
@@ -49,14 +58,14 @@ static size_t name_len(char *assignment)
 	return (i);
 }
 
-static t_error parse_assignment(char *str, t_assignment *out)
+static t_error	parse_assignment(char *str, t_assignment *out)
 {
-	size_t len;
+	size_t	len;
 
-	*out = (t_assignment) {0};
+	*out = (t_assignment){0};
 	len = name_len(str);
 	if (len == 0)
-		return NO_ERROR;
+		return (NO_ERROR);
 	out->name = ft_substr(str, 0, len);
 	if (!out->name)
 		return (E_OOM);
@@ -87,23 +96,23 @@ static t_error parse_assignment(char *str, t_assignment *out)
 		}
 		out->appending = false;
 	}
-	return NO_ERROR;
+	return (NO_ERROR);
 }
 
-static t_error assign_variable(t_env **env, t_assignment assignment)
+static t_error	assign_variable(t_env **env, t_assignment assignment)
 {
-	t_env_entry *entry;
-	char *joined;
+	t_env_entry	*entry;
+	char		*joined;
 
 	if (!env_key_exists(*env, assignment.name))
 		return (env_insert_owned_kv(env, assignment.name, assignment.value));
 	if (!assignment.value)
-		return NO_ERROR;
+		return (NO_ERROR);
 	entry = env_get_mut(*env, assignment.name);
 	assert(entry != NULL);
 	if (assignment.appending)
 	{
-		joined = ft_strjoin(!entry->value ? "" :  entry->value, assignment.value);
+		joined = ft_strjoin(!entry->value ? "" : entry->value, assignment.value);
 		if (!joined)
 			return (E_OOM);
 		free(assignment.value);
@@ -115,34 +124,34 @@ static t_error assign_variable(t_env **env, t_assignment assignment)
 	return (NO_ERROR);
 }
 
-t_command_result do_assignments(t_env **env, t_word_list *assignments)
+t_command_result	do_assignments(t_env **env, t_word_list *assignments)
 {
-	bool any_failed;
-	t_assignment assignment;
-	t_error err;
+	bool			any_failed;
+	t_assignment	assignment;
+	t_error			err;
 
 	any_failed = false;
 	while (assignments)
 	{
 		err = parse_assignment(assignments->contents, &assignment);
 		if (err != NO_ERROR)
-			return (t_command_result){.error = err};
+			return ((t_command_result){.error = err});
 		if (!assignment.name)
 		{
 			any_failed = true;
 			report_invalid_identifier(assignments->contents);
 			assignments = assignments->next;
-			continue;
+			continue ;
 		}
 		err = assign_variable(env, assignment);
 		if (err != NO_ERROR)
 		{
 			free(assignment.name);
 			free(assignment.value);
-			return (t_command_result){.error = err};
+			return ((t_command_result){.error = err});
 		}
 		assignments = assignments->next;
 	}
-	return (t_command_result) {.error = NO_ERROR, .status_code = any_failed ?
-								EXIT_FAILURE : EXIT_SUCCESS};
+	return ((t_command_result){.error = NO_ERROR,
+		.status_code = any_failed ? EXIT_FAILURE : EXIT_SUCCESS});
 }
