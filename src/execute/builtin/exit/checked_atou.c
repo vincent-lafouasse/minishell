96ba84d64062ccd881e9128e0439979824e361f9
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-22 16:47:53 +0100

    format builtin/exit/

--- src/execute/builtin/exit/checked_atou.c
+++ src/execute/builtin/exit/checked_atou.c
@@ -1,17 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   checked_atou.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 16:42:12 by poss              #+#    #+#             */
+/*   Updated: 2025/01/22 16:42:13 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/ctype.h"
 #include "libft/string.h"
-
 #include <stdbool.h>
 #include <stdint.h>
 
 #define STR_U32_MAX "4294967295"
 
-static bool    str_is_all_numbers(const char *s);
-static bool    will_overflow_u32(const char *s);
+static bool	str_is_all_numbers(const char *s);
+static bool	will_overflow_u32(const char *s);
 
-bool    checked_atou(const char *s, uint32_t *out)
+bool	checked_atou(const char *s, uint32_t *out)
 {
-	uint32_t    val;
+	uint32_t	val;
 
 	if (!s || !str_is_all_numbers(s) || !*s)
 		return (false);
@@ -29,7 +40,7 @@ bool    checked_atou(const char *s, uint32_t *out)
 	return (true);
 }
 
-static bool    str_is_all_numbers(const char *s)
+static bool	str_is_all_numbers(const char *s)
 {
 	if (!s)
 		return (false);
@@ -42,7 +53,7 @@ static bool    str_is_all_numbers(const char *s)
 	return (true);
 }
 
-static bool    will_overflow_u32(const char *s)
+static bool	will_overflow_u32(const char *s)
 {
 	if (ft_strlen(s) > ft_strlen(STR_U32_MAX))
 		return (true);
--- src/execute/builtin/exit/exit.c
+++ src/execute/builtin/exit/exit.c
@@ -1,46 +1,58 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/22 16:45:33 by poss              #+#    #+#             */
+/*   Updated: 2025/01/22 16:47:38 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../builtin.h"
-
-#include "execute/execute.h"
 #include "error/t_error.h"
-#include "word/t_word_list/t_word_list.h"
-#include "libft/string.h"
+#include "execute/execute.h"
 #include "libft/ctype.h"
-
-#include <unistd.h>
-#include <stdlib.h>
+#include "libft/string.h"
+#include "word/t_word_list/t_word_list.h"
 #include <stdbool.h>
+#include <stdlib.h>
+#include <unistd.h>
 
-bool    checked_atoi(const char *s, int32_t *out);
+#define NUMERIC_ARGUMENT_REQUIRED "minishell: exit: numeric argument required\n"
+#define TOO_MANY_ARGUMENTS "minishell: exit: too many arguments\n"
 
-_Noreturn
-static void exit_and_cleanup(t_state *state, int32_t exit_status)
+bool				checked_atoi(const char *s, int32_t *out);
+
+// bad, `exit.c:exit_and_cleanup` doesn't clean up before exiting
+_Noreturn static void	exit_and_cleanup(t_state *state, int32_t exit_status)
 {
 	(void)state;
-	exit(exit_status & 255); // bad, doesn't clean up before exiting
+	exit(exit_status & 255);
 }
 
-t_command_result execute_exit(t_state *state, t_simple *builtin)
+t_command_result	execute_exit(t_state *state, t_simple *builtin)
 {
-	t_word_list *args;
-	int32_t exit_status;
+	t_word_list	*args;
+	int32_t		exit_status;
 
 	if (state->is_interactive)
 		write(STDERR_FILENO, "exit\n", 5);
-
 	args = builtin->words->next;
 	if (!args)
 		exit_and_cleanup(state, state->last_status);
 	if (!checked_atoi(args->contents, &exit_status))
 	{
-		const char *error = "minishell: exit: numeric argument required\n";
-		write(STDERR_FILENO, error, ft_strlen(error));
+		write(STDERR_FILENO, NUMERIC_ARGUMENT_REQUIRED,
+			ft_strlen(NUMERIC_ARGUMENT_REQUIRED));
 		exit_and_cleanup(state, EX_BADUSAGE);
 	}
 	if (args->next != NULL)
 	{
-		const char *error = "minishell: exit: too many arguments\n";
-		write(STDERR_FILENO, error, ft_strlen(error));
-		return (t_command_result){.error = NO_ERROR, .status_code = EX_BADUSAGE};
+		write(STDERR_FILENO, TOO_MANY_ARGUMENTS, ft_strlen(TOO_MANY_ARGUMENTS));
+		return ((t_command_result){.error = NO_ERROR,
+			.status_code = EX_BADUSAGE});
 	}
 	exit_and_cleanup(state, exit_status);
 }
