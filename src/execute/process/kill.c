a640a0e4da4eff66380cf99c1e4698d87ec0af24
bitquence <imbitquence@gmail.com>
2025-01-26 21:11:02 +0100

    remove attempt_to_reap_processes method

--- src/execute/process/kill.c
+++ src/execute/process/kill.c
@@ -6,20 +6,6 @@
 #include <unistd.h>
 #include <signal.h>
 
-void	attempt_to_reap_processes()
-{
-	int status;
-
-	while (1)
-	{
-		status = waitpid(-1, NULL, WNOHANG);
-		if (status == 0 || (status == -1 && errno != EINTR))
-			break;
-	}
-	if (status == -1 && errno != ECHILD && errno != EINTR)
-		perror("minishell: kill_pipeline: waitpid");
-}
-
 void kill_pid_list(const t_pid_list *pids)
 {
 	const t_pid_list *curr;
@@ -27,14 +13,14 @@ void kill_pid_list(const t_pid_list *pids)
 	curr = pids;
 	while (curr)
 	{
-		kill(curr->pid, SIGKILL); // BAD: bash kills with SIGTERM followed by SIGCONT
+		kill(curr->pid, SIGKILL);
+		waitpid(curr->pid, NULL, WNOHANG);
 		curr = curr->next;
 	}
 }
 
 void	kill_pipeline(t_state *state, const t_pid_list *pids)
 {
-	attempt_to_reap_processes();
 	kill_pid_list(pids);
 	if (state->is_interactive)
 		reset_tty_properties(STDERR_FILENO, &state->tty_properties);
@@ -42,8 +28,8 @@ void	kill_pipeline(t_state *state, const t_pid_list *pids)
 
 void	kill_process(t_state *state, pid_t pid)
 {
-	attempt_to_reap_processes();
 	kill(pid, SIGKILL);
+	waitpid(pid, NULL, WNOHANG);
 	if (state->is_interactive)
 		reset_tty_properties(STDERR_FILENO, &state->tty_properties);
 }
