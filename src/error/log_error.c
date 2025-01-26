96507e9472f09473080f3461a3c2468cd929c572
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-26 20:35:56 +0100

    sketch report_error

--- src/error/log_error.c
+++ src/error/log_error.c
@@ -1,6 +1,27 @@
 #include "t_error.h"
+#include "libft/string.h"
+#include "libft/ft_io.h"
+#include "word/t_string/t_string.h"
 
+#include <unistd.h>
+
+// -> prints: "minishell: $ORIGIN: $REASON"
 void report_error(const char *origin, const char *reason) {
+    size_t len = ft_strlen(origin) + ft_strlen(reason) + 15;
+    t_string* msg = string_new_with_capacity(len);
+
+    if (!msg) {
+        ft_putstr_fd("minishell: ", STDERR_FILENO);
+        ft_putstr_fd(origin, STDERR_FILENO);
+        ft_putstr_fd(": ", STDERR_FILENO);
+        ft_putstr_fd(reason, STDERR_FILENO);
+    }
+
+    string_extend(&msg, "minishell: ");
+    string_extend(&msg, origin);
+    string_extend(&msg, ": ");
+    string_extend(&msg, reason);
+    ft_putstr_fd(msg->data, STDERR_FILENO);
 }
 
 void report_syscall_error(const char *syscall) {
