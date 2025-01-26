0385acc9e88d9d2c2e4a4fac2c9f662270a96394
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-26 20:49:53 +0100

    format

--- src/error/log_error.c
+++ src/error/log_error.c
@@ -1,47 +1,45 @@
-#include "t_error.h"
 #include "libft/ft_io.h"
 #include "libft/string.h"
+#include "t_error.h"
 #include "word/t_string/t_string.h"
-
 #include <string.h>
 #include <sys/errno.h>
 #include <unistd.h>
 
 // -> prints: "minishell: $ORIGIN: $REASON\n"
-void report_error(const char *origin, const char *reason)
+void	report_error(const char *origin, const char *reason)
 {
-    size_t len;
-    t_string *msg;
+	size_t		len;
+	t_string	*msg;
 
-    len = ft_strlen(origin) + ft_strlen(reason)
-        + ft_strlen("minishell: : \n");
-    msg = string_new_with_capacity(len);
-    if (!msg)
-    {
-        ft_putstr_fd("minishell: ", STDERR_FILENO);
-        ft_putstr_fd(origin, STDERR_FILENO);
-        ft_putstr_fd(": ", STDERR_FILENO);
-        ft_putstr_fd(reason, STDERR_FILENO);
-        ft_putstr_fd("\n", STDERR_FILENO);
-    }
-    string_extend(&msg, "minishell: ");
-    string_extend(&msg, origin);
-    string_extend(&msg, ": ");
-    string_extend(&msg, reason);
-    string_extend(&msg, "\n");
-    write(STDERR_FILENO, msg->data, msg->len);
+	len = ft_strlen(origin) + ft_strlen(reason) + ft_strlen("minishell: : \n");
+	msg = string_new_with_capacity(len);
+	if (!msg)
+	{
+		ft_putstr_fd("minishell: ", STDERR_FILENO);
+		ft_putstr_fd(origin, STDERR_FILENO);
+		ft_putstr_fd(": ", STDERR_FILENO);
+		ft_putstr_fd(reason, STDERR_FILENO);
+		ft_putstr_fd("\n", STDERR_FILENO);
+	}
+	string_extend(&msg, "minishell: ");
+	string_extend(&msg, origin);
+	string_extend(&msg, ": ");
+	string_extend(&msg, reason);
+	string_extend(&msg, "\n");
+	write(STDERR_FILENO, msg->data, msg->len);
 }
 
 // -> prints: "minishell: $SYSCALL: $STRERROR(ERRNO)\n"
-void report_syscall_error(const char *syscall)
+void	report_syscall_error(const char *syscall)
 {
-    const char *system_error = strerror(errno);
+	const char	*system_error = strerror(errno);
 
-    report_error(syscall, system_error);
+	report_error(syscall, system_error);
 }
 
 // -> prints: "minishell: $REASON:  $ERROR_REPR(err)\n"
-void report_t_error(const char *reason, t_error err)
+void	report_t_error(const char *reason, t_error err)
 {
-    report_error(reason, error_repr(err));
+	report_error(reason, error_repr(err));
 }
