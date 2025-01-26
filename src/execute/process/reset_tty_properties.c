36a0cc90d74344d4e472b576bd09c204a0638528
bitquence <imbitquence@gmail.com>
2025-01-26 22:17:20 +0100

    prefer `report_error` over manual write

--- src/execute/process/reset_tty_properties.c
+++ src/execute/process/reset_tty_properties.c
@@ -1,13 +1,12 @@
+#include "error/t_error.h"
 #include "process.h"
 
 #include "libft/ft_io.h"
 
 #include <termios.h>
 
-#define TCSETATTR_ERROR "minishell: tcsetattr: could not reset tty properties\n"
-
 void reset_tty_properties(int tty_fd, struct termios *tty_properties)
 {
 	if (tcsetattr(tty_fd, TCSADRAIN, tty_properties) < 0)
-		ft_putendl_fd(TCSETATTR_ERROR, STDERR_FILENO);
+		report_error("tcsetattr", "could not reset tty properties");
 }
