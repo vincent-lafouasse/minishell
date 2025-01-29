c871af5ddd304f98f248ed2db33722cea6dfb6fa
bitquence <imbitquence@gmail.com>
2025-01-29 19:18:18 +0100

    add missing shell cleanup C file

---
+++ src/shell/cleanup.c
@@ -0,0 +1,23 @@
+#include "shell.h"
+
+#include "parse/t_command/t_command.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+_Noreturn void shell_exit(t_state *state, int exit_status)
+{
+	shell_cleanup(state);
+	exit(exit_status);
+}
+
+void shell_cleanup(t_state *state)
+{
+	command_destroy_and_clear(&state->root);
+	free(state->line);
+	rl_clear_history();
+	env_destroy(&state->env);
+	pidl_clear(&state->our_children);
+}
