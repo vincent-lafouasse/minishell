29892962b33aca9aff24c972777b93912bee412a
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-30 19:33:16 +0100

    format

--- src/word/word.h
+++ src/word/word.h
@@ -1,8 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   word.h                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 19:32:49 by poss              #+#    #+#             */
+/*   Updated: 2025/01/30 19:32:54 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef WORD_H
 # define WORD_H
 
 # include "error/t_error.h"
 
-t_error word_clone(const char *word, char **out);
+t_error	word_clone(const char *word, char **out);
 
 #endif // WORD_H
