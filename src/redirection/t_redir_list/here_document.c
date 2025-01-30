b7e7350db6d46b6d60a931ab3025df61532acab2
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-30 20:00:33 +0100

    format

--- src/redirection/t_redir_list/here_document.c
+++ src/redirection/t_redir_list/here_document.c
@@ -1,16 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   here_document.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 19:54:05 by poss              #+#    #+#             */
+/*   Updated: 2025/01/30 19:56:30 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "error/t_error.h"
-#include "t_redir_list.h"
+#include "libft/string.h"
 #include "parse/t_command/t_command.h"
+#include "signal/signal.h"
+#include "t_redir_list.h"
 #include "word/expansions/expand.h"
 #include "word/t_string/t_string.h"
-#include "signal/signal.h"
-
-#include "libft/string.h"
-#include "libft/ft_io.h"
-#include "libft/ft_string.h"
 #include <stdio.h> // HACK: supress readline compilation errors on my end
 #include "readline/readline.h"
-
 #include <stdlib.h>
 #include <unistd.h>
 
@@ -25,17 +33,18 @@ static t_error	here_doc_append(t_string **out, const char *str)
 	return (NO_ERROR);
 }
 
-static bool delimiter_matches_line(const char *delimiter, const char *line)
+static bool	delimiter_matches_line(const char *delimiter, const char *line)
 {
-	size_t delimiter_len;
+	size_t	delimiter_len;
 
 	delimiter_len = ft_strlen(delimiter);
 	return (ft_strncmp(line, delimiter, delimiter_len + 1) == 0);
 }
 
-static t_error	read_here_document_internal(const char *delimiter, t_string **document_out)
+static t_error	read_here_document_internal(const char *delimiter,
+		t_string **document_out)
 {
-	char *line;
+	char	*line;
 
 	last_signal = 0;
 	while (1)
@@ -49,12 +58,12 @@ static t_error	read_here_document_internal(const char *delimiter, t_string **doc
 		if (!line)
 		{
 			report_error("warning", "here-document delimited by end-of-file");
-			break;
+			break ;
 		}
 		if (delimiter_matches_line(delimiter, line))
 		{
 			free(line);
-			break;
+			break ;
 		}
 		if (here_doc_append(document_out, line) != NO_ERROR)
 		{
@@ -66,11 +75,12 @@ static t_error	read_here_document_internal(const char *delimiter, t_string **doc
 	return (NO_ERROR);
 }
 
-static t_error	read_here_document(const char *raw_delimiter, char **document_out)
+static t_error	read_here_document(const char *raw_delimiter,
+		char **document_out)
 {
-	char *delimiter;
-	t_error err;
-	t_string *str;
+	char		*delimiter;
+	t_error		err;
+	t_string	*str;
 
 	err = quote_removed_word(raw_delimiter, &delimiter);
 	if (err != NO_ERROR)
@@ -93,21 +103,21 @@ static t_error	read_here_document(const char *raw_delimiter, char **document_out
 
 static t_error	read_here_documents_in_rdl(t_redir_list *rdl)
 {
-	t_here_doc *current_doc;
-	t_error err;
-	char *document;
+	t_here_doc	*current_doc;
+	t_error		err;
+	char		*document;
 
 	while (rdl)
 	{
 		if (rdl->redirect.kind != HERE_DOCUMENT)
 		{
 			rdl = rdl->next;
-			continue;
+			continue ;
 		}
 		current_doc = &rdl->redirect.doc;
 		err = read_here_document(current_doc->here_doc_eof, &document);
 		if (err != NO_ERROR)
-			return err;
+			return (err);
 		current_doc->contents = document;
 		rdl = rdl->next;
 	}
@@ -116,25 +126,26 @@ static t_error	read_here_documents_in_rdl(t_redir_list *rdl)
 
 t_error	gather_here_documents(t_command cmd)
 {
-	t_error err;
+	t_error	err;
 
 	if (cmd.type == CMD_SIMPLE)
-		return read_here_documents_in_rdl(cmd.simple->redirections);
+		return (read_here_documents_in_rdl(cmd.simple->redirections));
 	else if (cmd.type == CMD_SUBSHELL)
-		return gather_here_documents(cmd.subshell->cmd); // redirections ignored!
+		return (gather_here_documents(cmd.subshell->cmd));
+			// redirections ignored!
 	else if (cmd.type == CMD_PIPELINE)
 	{
 		err = gather_here_documents(cmd.pipeline->first);
 		if (err != NO_ERROR)
-			return err;
-		return gather_here_documents(cmd.pipeline->second);
+			return (err);
+		return (gather_here_documents(cmd.pipeline->second));
 	}
 	else if (cmd.type == CMD_CONDITIONAL)
 	{
 		err = gather_here_documents(cmd.conditional->first);
 		if (err != NO_ERROR)
-			return err;
-		return gather_here_documents(cmd.conditional->second);
+			return (err);
+		return (gather_here_documents(cmd.conditional->second));
 	}
 	return (NO_ERROR);
 }
--- src/redirection/t_redir_list/redirection_repr.c
+++ src/redirection/t_redir_list/redirection_repr.c
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirection_repr.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 19:54:41 by poss              #+#    #+#             */
+/*   Updated: 2025/01/30 19:54:42 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "t_redir_list.h"
 
-const char *redir_kind_repr(t_redir_kind kind)
+const char	*redir_kind_repr(t_redir_kind kind)
 {
 	if (kind == FROM_FILE)
-		return "<";
+		return ("<");
 	else if (kind == INTO_FILE)
-		return ">";
+		return (">");
 	else if (kind == APPEND_INTO_FILE)
-		return ">>";
+		return (">>");
 	else if (kind == HERE_DOCUMENT)
-		return "<<";
-	return "Unknown redirection: ";
+		return ("<<");
+	return ("Unknown redirection: ");
 }
