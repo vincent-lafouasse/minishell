950c279e05dc45d74bd306c681b6f6e3a1213160
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-30 19:31:52 +0100

    extract functions into side file

--- src/parse/t_command/command_destroy.c
+++ src/parse/t_command/command_destroy.c
@@ -1,55 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   destructor.c                                       :+:      :+:    :+:   */
+/*   command_destroy.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/12 13:57:53 by poss              #+#    #+#             */
-/*   Updated: 2024/11/12 13:57:53 by poss             ###   ########.fr       */
+/*   Updated: 2025/01/30 19:31:39 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "redirection/t_redir_list/t_redir_list.h"
 #include "t_command.h"
-#include "word/t_word_list/t_word_list.h"
-#include <stdlib.h>
 
-void	pipeline_destroy(t_pipeline *pipeline)
-{
-	if (pipeline == NULL)
-		return ;
-	command_destroy(pipeline->first);
-	command_destroy(pipeline->second);
-	free(pipeline);
-}
-
-void	conditional_destroy(t_conditional *conditional)
-{
-	if (conditional == NULL)
-		return ;
-	command_destroy(conditional->first);
-	command_destroy(conditional->second);
-	free(conditional);
-}
-
-void	subshell_destroy(t_subshell *subshell)
-{
-	if (subshell == NULL)
-		return ;
-	command_destroy(subshell->cmd);
-	rdl_clear(&subshell->redirections, free);
-	free(subshell);
-}
-
-void	simple_destroy(t_simple *simple)
-{
-	if (simple == NULL)
-		return ;
-	rdl_clear(&simple->redirections, free);
-	wl_clear(&simple->words, free);
-	free(simple);
-}
+void	pipeline_destroy(t_pipeline *pipeline);
+void	conditional_destroy(t_conditional *conditional);
+void	subshell_destroy(t_subshell *subshell);
+void	simple_destroy(t_simple *simple);
 
 void	command_destroy_and_clear(t_command *command)
 {
