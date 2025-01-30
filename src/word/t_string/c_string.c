241ca21b02747b89a27345435c89972dd5c3b071
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-30 19:34:24 +0100

    format

--- src/word/t_string/c_string.c
+++ src/word/t_string/c_string.c
@@ -1,12 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   c_string.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 19:34:07 by poss              #+#    #+#             */
+/*   Updated: 2025/01/30 19:34:09 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft/string.h"
 #include "t_string.h"
-
 #include <stdlib.h>
 
-t_error		string_make_c_string(const t_string *self, char **out)
+t_error	string_make_c_string(const t_string *self, char **out)
 {
-	char *new;
-	size_t len;
+	char	*new;
+	size_t	len;
 
 	len = self->len;
 	new = malloc((len + 1) * sizeof(char));
