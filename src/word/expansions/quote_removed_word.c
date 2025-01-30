33e67062535cc51bf0b29b0d8f3d9dec70de6017
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-30 19:50:29 +0100

    format

--- src/word/expansions/quote_removed_word.c
+++ src/word/expansions/quote_removed_word.c
@@ -12,13 +12,12 @@
 
 #include "./expand.h"
 #include "./expand_internals.h"
-
 #include "error/t_error.h"
 
-t_error quote_removed_word(const char *word, char **out)
+t_error	quote_removed_word(const char *word, char **out)
 {
-	t_error err;
-	t_word_quotes_list *parts;
+	t_error				err;
+	t_word_quotes_list	*parts;
 
 	err = wql_parse(word, &parts);
 	if (err != NO_ERROR)
