ac8debfccf5f715ce827eef95468926b03094a39
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-30 19:51:58 +0100

    format

--- src/error/error_repr.c
+++ src/error/error_repr.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 19:07:24 by poss              #+#    #+#             */
-/*   Updated: 2025/01/30 19:07:27 by poss             ###   ########.fr       */
+/*   Updated: 2025/01/30 19:51:49 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,49 +36,49 @@
 
 #define UNKNOWN_ERROR_REPR "Unknown error"
 
-const char *error_repr(t_error err)
+const char	*error_repr(t_error err)
 {
-    if (err == NO_ERROR)
-        return NO_ERROR_REPR;
-    if (err == E_UNRECOGNIZED_TOKEN)
-        return UNRECOGNIZED_TOKEN_REPR;
-    if (err == E_UNEXPECTED_TOKEN)
-        return UNEXPECTED_TOKEN_REPR;
-    if (err == E_UNTERMINATED_QUOTE)
-        return UNTERMINATED_QUOTE_REPR;
-    if (err == E_OOM)
-        return OUT_OF_MEMORY_REPR;
-    if (err == E_UNREACHABLE)
-        return UNREACHABLE_REPR;
-    if (err == E_NULL_PARAM)
-        return NULL_PARAM_REPR;
-    if (err == E_MALFORMED_ENVP)
-        return MALFORMED_ENVP_REPR;
-    if (err == E_COMMAND_NOT_FOUND)
-        return COMMAND_NOT_FOUND_REPR;
-    if (err == E_FORK)
-        return FORK_REPR;
-    if (err == E_DUP2)
-        return DUP2_REPR;
-    if (err == E_DUMMY)
-        return DUMMY_REPR;
-    if (err == E_ACCESS)
-        return ACCESS_REPR;
-    if (err == E_INTERRUPTED)
-        return INTERRUPTED_REPR;
-    if (err == E_GETCWD)
-        return GETCWD_REPR;
-    if (err == E_WRITE)
-        return WRITE_REPR;
-    if (err == E_WAIT)
-        return WAIT_REPR;
-    if (err == E_OPEN)
-        return OPEN_REPR;
-    if (err == E_UNLINK)
-        return UNLINK_REPR;
-    if (err == E_PIPE)
-        return PIPE_REPR;
-    if (err == E_AMBIGUOUS_REDIRECT)
-        return AMBIGUOUS_REDIRECT_REPR;
-    return UNKNOWN_ERROR_REPR;
+	if (err == NO_ERROR)
+		return (NO_ERROR_REPR);
+	if (err == E_UNRECOGNIZED_TOKEN)
+		return (UNRECOGNIZED_TOKEN_REPR);
+	if (err == E_UNEXPECTED_TOKEN)
+		return (UNEXPECTED_TOKEN_REPR);
+	if (err == E_UNTERMINATED_QUOTE)
+		return (UNTERMINATED_QUOTE_REPR);
+	if (err == E_OOM)
+		return (OUT_OF_MEMORY_REPR);
+	if (err == E_UNREACHABLE)
+		return (UNREACHABLE_REPR);
+	if (err == E_NULL_PARAM)
+		return (NULL_PARAM_REPR);
+	if (err == E_MALFORMED_ENVP)
+		return (MALFORMED_ENVP_REPR);
+	if (err == E_COMMAND_NOT_FOUND)
+		return (COMMAND_NOT_FOUND_REPR);
+	if (err == E_FORK)
+		return (FORK_REPR);
+	if (err == E_DUP2)
+		return (DUP2_REPR);
+	if (err == E_DUMMY)
+		return (DUMMY_REPR);
+	if (err == E_ACCESS)
+		return (ACCESS_REPR);
+	if (err == E_INTERRUPTED)
+		return (INTERRUPTED_REPR);
+	if (err == E_GETCWD)
+		return (GETCWD_REPR);
+	if (err == E_WRITE)
+		return (WRITE_REPR);
+	if (err == E_WAIT)
+		return (WAIT_REPR);
+	if (err == E_OPEN)
+		return (OPEN_REPR);
+	if (err == E_UNLINK)
+		return (UNLINK_REPR);
+	if (err == E_PIPE)
+		return (PIPE_REPR);
+	if (err == E_AMBIGUOUS_REDIRECT)
+		return (AMBIGUOUS_REDIRECT_REPR);
+	return (UNKNOWN_ERROR_REPR);
 }
