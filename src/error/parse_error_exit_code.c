0b6d440a526be0f8e3edd0ef956b78ac66292960
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-30 19:10:45 +0100

    format most of ./error/, error_repr todo

--- src/error/parse_error_exit_code.c
+++ src/error/parse_error_exit_code.c
@@ -1,9 +1,21 @@
-#include "t_error.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_error_exit_code.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 19:07:40 by poss              #+#    #+#             */
+/*   Updated: 2025/01/30 19:07:42 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
+#include "t_error.h"
 #include <stdlib.h>
 
-char parse_error_exit_code(t_error err) {
-    if (err == E_UNEXPECTED_TOKEN || err == E_UNRECOGNIZED_TOKEN)
-        return 2;
-    return EXIT_FAILURE;
+char	parse_error_exit_code(t_error err)
+{
+	if (err == E_UNEXPECTED_TOKEN || err == E_UNRECOGNIZED_TOKEN)
+		return (2);
+	return (EXIT_FAILURE);
 }
--- src/error/t_error.h
+++ src/error/t_error.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   t_error.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/30 19:07:44 by poss              #+#    #+#             */
+/*   Updated: 2025/01/30 19:07:45 by poss             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #pragma once
 
 #include <stdbool.h>
@@ -5,36 +17,36 @@
 /* Update error_repr.c when adding a new variant */
 typedef enum e_error
 {
-    NO_ERROR,
-    E_UNRECOGNIZED_TOKEN,
-    E_UNEXPECTED_TOKEN,
-    E_UNTERMINATED_QUOTE,
-    E_OOM,
-    E_UNREACHABLE,
-    E_NULL_PARAM,
-    E_MALFORMED_ENVP,
-    E_COMMAND_NOT_FOUND,
-    E_FORK,
-    E_DUP2,
-    E_EXECVE,
-    E_ACCESS,
-    E_GETCWD,
-    E_WRITE,
-    E_WAIT,
-    E_OPEN,
-    E_UNLINK,
-    E_PIPE,
-    E_DUMMY,
-    E_AMBIGUOUS_REDIRECT,
-    E_INTERRUPTED,
-} t_error;
+	NO_ERROR,
+	E_UNRECOGNIZED_TOKEN,
+	E_UNEXPECTED_TOKEN,
+	E_UNTERMINATED_QUOTE,
+	E_OOM,
+	E_UNREACHABLE,
+	E_NULL_PARAM,
+	E_MALFORMED_ENVP,
+	E_COMMAND_NOT_FOUND,
+	E_FORK,
+	E_DUP2,
+	E_EXECVE,
+	E_ACCESS,
+	E_GETCWD,
+	E_WRITE,
+	E_WAIT,
+	E_OPEN,
+	E_UNLINK,
+	E_PIPE,
+	E_DUMMY,
+	E_AMBIGUOUS_REDIRECT,
+	E_INTERRUPTED,
+}			t_error;
 
-const char *error_repr(t_error err);
-bool is_syscall_related(t_error err);
-bool is_fatal_error(t_error err);
-char parse_error_exit_code(t_error err);
+const char	*error_repr(t_error err);
+bool		is_syscall_related(t_error err);
+bool		is_fatal_error(t_error err);
+char		parse_error_exit_code(t_error err);
 
-void report_error_message(const char *reason);
-void report_error(const char *origin, const char *reason);
-void report_syscall_error(const char *syscall);
-void report_t_error(const char *origin, t_error err);
+void		report_error_message(const char *reason);
+void		report_error(const char *origin, const char *reason);
+void		report_syscall_error(const char *syscall);
+void		report_t_error(const char *origin, t_error err);
