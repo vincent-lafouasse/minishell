613b7d61fae699e65ecf7dd2e1535235825b69c3
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2025-01-30 19:17:18 +0100

    format log.h

--- src/log/log.h
+++ src/log/log.h
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 19:11:27 by poss              #+#    #+#             */
-/*   Updated: 2025/01/30 19:11:41 by poss             ###   ########.fr       */
+/*   Updated: 2025/01/30 19:16:52 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,10 @@
 #include "word/t_word_list/t_word_list.h"
 #include "redirection/t_redir_list/t_redir_list.h"
 
-void log_token(t_token token);
-void log_token_list(const t_token_list *tkl);
-void parse_tree_to_json(const t_symbol *root);
-void syntax_tree_to_json(t_command tree);
-void log_word_list(const t_word_list *words);
-void log_redir_list(const t_redir_list *rdl);
-void log_env(const t_env *env);
+void	log_token(t_token token);
+void	log_token_list(const t_token_list *tkl);
+void	parse_tree_to_json(const t_symbol *root);
+void	syntax_tree_to_json(t_command tree);
+void	log_word_list(const t_word_list *words);
+void	log_redir_list(const t_redir_list *rdl);
+void	log_env(const t_env *env);
