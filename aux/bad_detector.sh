9267cd31672e2fa9dc94858152498128b52336d6
bitquence <imbitquence@gmail.com>
2025-02-01 22:33:24 +0100

    remove makefile indicators from bad detector

--- aux/bad_detector.sh
+++ aux/bad_detector.sh
@@ -3,17 +3,5 @@
 PATTERNS=(assert bad xxx dummy hack todo temporarily '#include <assert.h>' pragma)
 
 for pattern in "${PATTERNS[@]}"; do
-	grep --recursive --ignore-case "${pattern}" ./src
+	grep -n --recursive --ignore-case "${pattern}" ./src
 done
-
-MAKEFILE_HAS_WERROR=$(grep '^CFLAGS += -Werror' Makefile)
-if [ -z "${MAKEFILE_HAS_WERROR}" ];
-then
-	echo "Makefile doesn't compile with -Werror"
-fi
-
-MAKEFILE_HAS_WILDCARD=$(grep find Makefile)
-if [ -n "${MAKEFILE_HAS_WILDCARD}" ];
-then
-	echo "Makefile uses wildcard to list sources"
-fi
